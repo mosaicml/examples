Fix mosaic_bert attention score scaling

bert_pretrain/src/bert_layers.py
@@ -177,7 +177,7 @@ class BertUnpadSelfAttention(nn.Module):
         q = qkv[:, :, 0, :, :].permute(0, 2, 1, 3)  # b h s d
         k = qkv[:, :, 1, :, :].permute(0, 2, 3, 1)  # b h d s
         v = qkv[:, :, 2, :, :].permute(0, 2, 1, 3)  # b h s d
-        attention_scores = torch.matmul(q, k)
+        attention_scores = torch.matmul(q, k) / math.sqrt(self.attention_head_size)
         attention_scores = attention_scores + alibi_attn_mask
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
         attention_probs = self.dropout(attention_probs)