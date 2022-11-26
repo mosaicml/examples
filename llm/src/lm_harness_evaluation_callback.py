a77df03c1686f5db62ce5e1359de8822cf19d90e
evanfabry <evan.fabry@gmail.com>
2022-11-26 09:52:36 -0800

    batch size 64

--- llm/src/lm_harness_evaluation_callback.py
+++ llm/src/lm_harness_evaluation_callback.py
@@ -166,7 +166,7 @@ class EvaluationCallback(Callback):
                     ),
                     "precision": state.precision,
                     "device": torch.device('cuda').type,
-                    "batch_size": 512,
+                    "batch_size": 64,
                 }
             )
             main(
