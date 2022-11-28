2349bad6323fd963c9aa6da7093e1ea39be5c0ef
evanfabry <evan.fabry@gmail.com>
2022-11-28 17:25:24 -0600

    bump batch size to 256

--- llm/src/lm_harness_evaluation_callback.py
+++ llm/src/lm_harness_evaluation_callback.py
@@ -166,7 +166,7 @@ class EvaluationCallback(Callback):
                     ),
                     "precision": state.precision,
                     "device": torch.device('cuda').type,
-                    "batch_size": 64,
+                    "batch_size": 256,
                 }
             )
             main(
