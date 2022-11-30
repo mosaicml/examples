ff9c53d29dea352e3db4c81eab68e46b47fb9570
evanfabry <evan.fabry@gmail.com>
2022-11-30 01:26:24 -0600

    don't try to force to gpu

--- llm/src/lm_harness_evaluation_callback.py
+++ llm/src/lm_harness_evaluation_callback.py
@@ -156,12 +156,12 @@ class LMEvalHarnessReducerMetric(torchmetrics.Metric):
     """
     def __init__(self):
         super().__init__()
-        self.add_state("responses", default=torch.Tensor([]).cuda(), dist_reduce_fx="cat")
-        self.add_state("sampled_indices", default=torch.Tensor([]).cuda(), dist_reduce_fx="cat")
+        self.add_state("responses", default=torch.Tensor([]), dist_reduce_fx="cat")
+        self.add_state("sampled_indices", default=torch.Tensor([]), dist_reduce_fx="cat")
 
     def update(self, preds, target):
-        self.responses = torch.cat([self.responses, torch.Tensor(preds)]).cuda()
-        self.sampled_indices = torch.cat([self.sampled_indices, torch.Tensor(target)]).cuda()
+        self.responses = torch.cat([self.responses, torch.Tensor(preds)])
+        self.sampled_indices = torch.cat([self.sampled_indices, torch.Tensor(target)])
         print(f"metric updating... new shape: {self.responses.shape}")
 
     def compute(self):
