822ef60309cb1bc15e3219913c5261a04b0a6d52
Davis Blalock <davis@mosaicml.com>
2022-12-01 22:50:06 -0800

    rm unused and probably bugged flash attn Function variant

--- bert/src/flash_attn_triton.py
+++ bert/src/flash_attn_triton.py
@@ -1041,58 +1041,6 @@ class FlashAttnQKVPackedFunc(torch.autograd.Function):
 flash_attn_qkvpacked_func = FlashAttnQKVPackedFunc.apply
 
 
-class FlashAttnKVPackedFunc(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, q, kv, bias=None, causal=False, softmax_scale=None):
-        """
-            q: (batch, seqlen_q, nheads, headdim)
-            kv: (batch, seqlen_k, 2, nheads, headdim)
-            bias: optional, shape broadcastible to (batch, nheads, seqlen_q, seqlen_k).
-                For example, ALiBi mask for causal would have shape (1, nheads, 1, seqlen_k).
-                ALiBi mask for non-causal would have shape (1, nheads, seqlen_q, seqlen_k)
-        """
-        # Make sure that the last dimension is contiguous
-        q, kv = [x if x.stride(-1) == 1 else x.contiguous() for x in [q, kv]]
-        o, lse, ctx.softmax_scale = _flash_attn_forward(
-            q,
-            kv[:, :, 0],
-            kv[:, :, 1],
-            bias=bias,
-            causal=causal,
-            softmax_scale=softmax_scale)
-        ctx.save_for_backward(q, kv, o, lse, bias)
-        ctx.causal = causal
-        return o
-
-    @staticmethod
-    def backward(ctx, do):
-        q, kv, o, lse, bias = ctx.saved_tensors
-        assert not ctx.needs_input_grad[
-            2], 'FlashAttention does not support bias gradient yet'
-        # Triton's autotune causes the Tensor._version to change, and so Pytorch autograd
-        # does a memcpy. To avoid this we run in inference_mode, which doesn't track the version.
-        with torch.inference_mode():
-            dq = torch.empty_like(q)
-            dkv = torch.empty_like(kv)
-            _flash_attn_backward(do,
-                                 q,
-                                 qkv[:, :, 0],
-                                 qkv[:, :, 1],
-                                 o,
-                                 lse,
-                                 dq,
-                                 dkv[:, :, 0],
-                                 dkv[:, :, 1],
-                                 bias=bias,
-                                 causal=ctx.causal,
-                                 softmax_scale=ctx.softmax_scale)
-        return dq, dkv, None, None, None
-
-
-flash_attn_kvpacked_func = FlashAttnKVPackedFunc.apply
-
-
 class FlashAttnFunc(torch.autograd.Function):
 
     @staticmethod
