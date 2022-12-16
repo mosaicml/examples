3b3ccf37cfe03c79e7869aa4eb03c0914280f67d
Davis Blalock <davis@mosaicml.com>
2022-12-16 02:06:06 +0000

    clean up benchmark test script

--- scripts/test_benchmark.sh
+++ scripts/test_benchmark.sh
@@ -15,40 +15,21 @@ ENV_NAME="${1%/}-env"   # strip trailing slash if present
 
 cd "$1"
 
-# if [ "$2" ]; then
 echo "Creating venv..."
 python -m venv "$ENV_NAME" --system-site-packages
 source "$ENV_NAME/bin/activate"
-# fi
-
-# # work around flash-attn needing torch already installed in its setup.py
-# cat requirements.txt | grep 'torch' | xargs pip install
 
 cat requirements.txt | grep -v 'flash-attn' > /tmp/requirements.txt
-# cat requirements.txt | grep -Ev 'flash-attn|mm' > /tmp/requirements.txt
 
 echo "Installing requirements:"
 cat /tmp/requirements.txt
-# cd -; exit
-# pip install -I -r /tmp/requirements.txt
 pip install -U -r /tmp/requirements.txt
 rm /tmp/requirements.txt
 
-# pip install -U -r requirements.txt
-
-pip show flash-attn
-python -c 'import torch; print(torch.__path__[0])'
-python -c 'import numpy; print(numpy.__path__[0])'
-python -c 'import flash_attn; print(flash_attn.__path__[0])'
-# exit
-
-
 pytest tests/*
 
-# if [ "$2" ]; then
 echo "Cleaning up venv..."
 deactivate
 rm -rf "$ENV_NAME"
-# fi
 
 cd -
