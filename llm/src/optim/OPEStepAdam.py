076307cf19cc088c65bd2733791c1e0ca7abb2ce
bmosaicml <jeremy@mosaicml.com>
2022-12-17 16:52:08 -0500

    add ope step lr slowdown

--- llm/src/optim/OPEStepAdam.py
+++ llm/src/optim/OPEStepAdam.py
@@ -3,27 +3,25 @@ from __future__ import annotations
 from typing import Iterable, List, Tuple, Union
 from composer.optim import DecoupledAdamW
 import torch
-
+import random
 class OnlinePercentileEstimate:
-    def __init__(self, percentile=0.85, step=0.5, initial_estimate=0.2):
-        self.step = step
+    def __init__(self, percentile=0.90):
+        self.step = None
         self.step_up = 1.0 - percentile
-        self.step_down = percentile
-        self.percentile_estimate = initial_estimate
-        self.skip_counter = 0
+        self.target_percentile = percentile
+        self.percentile_estimate = None
+        self.outlier_counter = 0
 
     def push(self, observation):
         if self.percentile_estimate is None:
             self.percentile_estimate = observation
-            self.step = max(abs(self.percentile_estimate/2), self.step)
-            return
+            self.step = abs(self.percentile_estimate/2)
 
         if self.percentile_estimate > observation:
             self.percentile_estimate -= self.step * self.step_up
         elif self.percentile_estimate < observation:
-            self.percentile_estimate += self.step * self.step_down
-        if abs(observation - self.percentile_estimate) < self.step:
-            self.step /= 2.0
+            self.percentile_estimate += self.step * self.target_percentile
+        self.step = abs(observation - self.percentile_estimate) / 2
 
     def query_threshold(self):
         return self.percentile_estimate
@@ -39,21 +37,26 @@ class OPEStepAdam(DecoupledAdamW):
                 weight_decay: float = 1e-5,
                 percentile_cutoff: float = 0.85,
                 warmup: int = 5000,
-                amsgrad: bool = False):
+                amsgrad: bool = False,
+                skip_outliers: bool = False,
+                lr_decay: float = 0.02):
         super().__init__(params=params, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=amsgrad)
         for group in self.param_groups:
             group['initial_lr'] = group['lr']
         self.target_percentile_cutoff = percentile_cutoff
         self.warmup = max(warmup, 1)
+        self.skip_outliers = skip_outliers
+        self.lr_decay = lr_decay
         
 
     
     @staticmethod
     def adam_step(params: List[torch.Tensor], grads: List[torch.Tensor], exp_avgs: List[torch.Tensor],
-              exp_avg_sqs: List[torch.Tensor], max_exp_avg_sqs: List[torch.Tensor], state_steps: List[int], 
-              online_percentile_estimates: List[OnlinePercentileEstimate], *,
-              beta1: float, beta2: float, lr: float, initial_lr: float, weight_decay: float,
-              eps: float, warmup: int, amsgrad:bool) -> None:
+              exp_avg_sqs: List[torch.Tensor], max_exp_avg_sqs: List[torch.Tensor], layerwise_lrs: List[torch.Tensor],
+              state_steps: List[int], online_percentile_estimates: List[OnlinePercentileEstimate], *,
+              beta1: float, beta2: float, initial_lr: float, weight_decay: float,
+              eps: float, warmup: int, amsgrad:bool, skip_outliers: bool,
+              lr_decay: bool) -> None:
         r"""Functional API that performs AdamW algorithm computation with decoupled weight decay.
 
         Args:
@@ -66,7 +69,6 @@ class OPEStepAdam(DecoupledAdamW):
             amsgrad (bool): Enables amsgrad variant of Adam.
             beta1 (float): Coefficient for computing the moving average of gradient values.
             beta2 (float): Coefficient for computing the moving average of squared gradient values.
-            lr (float): Learning rate.
             initial_lr (float): Initial learning rate.
             weight_decay (float): Factor for decoupled weight decay
             eps (float): Term added to the denominator to improve numerical stability.
@@ -78,12 +80,8 @@ class OPEStepAdam(DecoupledAdamW):
             exp_avg = exp_avgs[i]
             exp_avg_sq = exp_avg_sqs[i]
             step = state_steps[i]
+            layerwise_lr = layerwise_lrs[i]
           
-            # Perform stepweight decay
-            if weight_decay != 0:
-                decay_factor = (lr / initial_lr) if initial_lr else 1.0
-                param.mul_(1 - decay_factor * weight_decay)
-
             if step == 1:
                 beta1 = 0
                 beta2 = 0
@@ -99,15 +97,34 @@ class OPEStepAdam(DecoupledAdamW):
             else:
                 update = (exp_avg_sq.sqrt()).add_(eps)
 
-            update.pow_(-1).mul_(-lr*exp_avg)
+            # calculate the gradient-based update
+            update.pow_(-1).mul_(-layerwise_lr*exp_avg)
+
+            # Perform stepweight decay
+            if weight_decay != 0:
+                decay_factor = (layerwise_lr.item() / initial_lr) if initial_lr else 1.0
+                update.add_(param, alpha=-decay_factor * weight_decay)
+
             update_norm = torch.linalg.vector_norm(update)
-            percentile_cutoff = ope.query_threshold()
             ope.push(update_norm)
+            percentile_cutoff = ope.query_threshold()
             if update_norm > percentile_cutoff and step > warmup:
-                ope.skip_counter += 1
-                continue
-            else:
-                param.add_(update)
+                # if we are over the percentile always decrease LR
+                ope.outlier_counter += 1
+                if skip_outliers:
+                    continue
+                else:
+                    layerwise_lr.mul_(1-lr_decay)
+            elif update_norm < percentile_cutoff and step > warmup and random.random() > ope.target_percentile:
+                # if we are under the percentile, decrease it only 1-percentile pctg of the time
+                # i.e. if percentile is 95%, decrease the percentile only 5% of the time
+                if skip_outliers:
+                    continue
+                else:
+                    layerwise_lr.mul_(1+lr_decay)
+            breakpoint()
+
+            param.add_(update)
           
 
     @torch.no_grad()
@@ -128,14 +145,14 @@ class OPEStepAdam(DecoupledAdamW):
             online_percentile_estimates = []
             grads = []
             exp_avgs = []
+            layerwise_lrs = []
             exp_avg_sqs = []
             max_exp_avg_sqs = []
             state_steps = []
             beta1, beta2 = group['betas']
             amsgrad = group['amsgrad']
             eps = group['eps']
-            lr = group['lr']
-            initial_lr = group['initial_lr']
+            initial_lr = group['lr']
             weight_decay = group['weight_decay']
 
             for p in group['params']:
@@ -160,9 +177,10 @@ class OPEStepAdam(DecoupledAdamW):
                         state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
                     state['ope'] = OnlinePercentileEstimate(self.target_percentile_cutoff)
+                    state['lr'] = torch.tensor(initial_lr)
                 
                 online_percentile_estimates.append(state['ope'])
-                    
+                layerwise_lrs.append(state['lr'])
                 exp_avgs.append(state['exp_avg'])
                 exp_avg_sqs.append(state['exp_avg_sq'])
                 if amsgrad:
@@ -177,16 +195,18 @@ class OPEStepAdam(DecoupledAdamW):
                        exp_avgs,
                        exp_avg_sqs,
                        max_exp_avg_sqs,
+                       layerwise_lrs,
                        state_steps,
                        online_percentile_estimates,
                        beta1=beta1,
                        beta2=beta2,
-                       lr=lr,
                        initial_lr=initial_lr,
                        weight_decay=weight_decay,
                        eps=eps,
                        warmup=self.warmup,
-                       amsgrad=amsgrad)
+                       amsgrad=amsgrad,
+                       skip_outliers=self.skip_outliers,
+                       lr_decay=self.lr_decay)
 
         return loss
 
@@ -195,7 +215,9 @@ class OPEStepAdam(DecoupledAdamW):
         optimizer_metrics = super().report_per_parameter_metrics(param, name, optimizer_metrics)
         if param in self.state:
             param_optim_state = self.state[param]
-            optimizer_metrics[f"outlier/threshold_estimate/{name}"] = param_optim_state['ope'].query_threshold()
-            optimizer_metrics[f"outlier/skip_counter/{name}"] = param_optim_state['ope'].skip_counter
+            optimizer_metrics[f"layerwise_lr/{name}"] = param_optim_state['lr']
+            optimizer_metrics[f"ope/threshold_estimate/{name}"] = param_optim_state['ope'].query_threshold()
+            optimizer_metrics[f"ope/outlier_counter/{name}"] = param_optim_state['ope'].outlier_counter
+            optimizer_metrics[f"ope/ope_step_size/{name}"] = param_optim_state['ope'].step
 
         return optimizer_metrics
