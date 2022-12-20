0d6cfdc3b54848d292b12ffc12868d8137dbc63a
Vitaliy Chiley <vitaliy@mosaicml.com>
2022-12-19 23:01:21 -0800

    updt

--- llm/src/text_data.py
+++ llm/src/text_data.py
@@ -195,6 +195,7 @@ def build_text_dataloader(cfg: DictConfig, device_batch_size: int):
 
 
 # Helpful to test if your dataloader is working locally
+# Note this currently does not work on MacOS (Jira: CO-1550)
 # Run `python data.py [remote] [local, optional]` and verify that batches are printed out
 if __name__ == '__main__':
     remote = sys.argv[1]
