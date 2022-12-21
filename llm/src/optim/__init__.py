468fc388a7b2dda739ac3cf39e7301725f8e5a88
bmosaicml <jeremy@mosaicml.com>
2022-12-21 00:41:12 -0500

    add second order adalr

--- llm/src/optim/__init__.py
+++ llm/src/optim/__init__.py
@@ -2,6 +2,7 @@
 from src.optim.OPEStepAdam import OPEStepAdam
 from src.optim.AdaBound import AdaBound
 from src.optim.EMASmoothStepAdam import EMASmoothStepAdam
+from src.optim.SecondOrderAdaLR import SecondOrderAdaLR
 from composer.optim import DecoupledAdamW
 
 __all__ = [
@@ -13,5 +14,5 @@ OPTIMIZERS = {
     "ope_step_adam": OPEStepAdam,
     "ema_step_adam": EMASmoothStepAdam,
     "adabound": AdaBound,
-
+    "second_order_adalr": SecondOrderAdaLR
 }
