4eb12104decad16a4d14f59ffed29e0dfefd095d
Vitaliy Chiley <vitaliy@mosaicml.com>
2022-12-22 14:41:26 -0800

    updt

--- llm/src/mosaic_gpt.py
+++ llm/src/mosaic_gpt.py
@@ -147,8 +147,6 @@ class TritonFlashCausalAttention(nn.Module):
         self.attn_bias_initialized = True
 
     def forward(self, x, key_padding_mask):
-        if key_padding_mask.count_nonzero() != key_padding_mask.numel():
-            raise NotImplementedError(f'TritonFlashCausalAttention does not ignore input tokens')
         if not self.attn_bias_initialized:
             self._fill_attn_bias()
 
