2f6d663a970a520fdbbc81d02f16ec21ddf059c1
Vitaliy Chiley <vitaliy@mosaicml.com>
2022-12-30 07:45:34 -0800

    updt

--- llm/main.py
+++ llm/main.py
@@ -155,7 +155,8 @@ def main(cfg):
     model = build_composer_model(cfg.model)
     cfg.n_params = sum(p.numel() for p in model.parameters())
     print(f'{cfg.n_params=:.2e}')
-    print(f'{model.num_fwd_flops=:.2e}')
+    if hasattr(model, 'num_fwd_flops'):
+        print(f'{model.num_fwd_flops=:.2e}')
 
     # Dataloaders
     print('Building train loader...')
