ba09ae3713a090c13b5170cb329e3925d3b2adc1
austin <jacobsonaustinj@gmail.com>
2023-01-20 23:42:44 -0800

    typo

--- stable-diffusion/model.py
+++ stable-diffusion/model.py
@@ -194,7 +194,7 @@ class StableDiffusion(ComposerModel):
         latents = 1 / 0.18215 * latents
         image = self.vae.decode(latents).sample
         image =  (image / 2 + 0.5).clamp(0, 1)
-        return image.detach.cpu()
+        return image.detach().cpu()
         # image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
         # return (image * 255).round().astype("uint8")
 
