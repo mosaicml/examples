7e10f3aad2b9dcca3fc6db69882b868b56019e19
Abhinav Venigalla <abhi@mosaicml.com>
2023-01-26 01:27:03 +0000

    wip

--- common/__init__.py
+++ common/__init__.py
@@ -1,12 +1,9 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = '0.0.2'
-
 from . import builders, logging_utils, speed_monitor_w_mfu, text_data
 
 __all__ = [
-    '__version__',
     'builders',
     'text_data',
     'speed_monitor_w_mfu',
