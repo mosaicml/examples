20ff60f55640efb17ab916334908b4ea413036bd
Alex Trott <alex@mosaicml.com>
2023-01-30 17:36:51 -0800

    Refactor batch size utility into common

--- examples/bert/main.py
+++ examples/bert/main.py
@@ -16,46 +16,7 @@ from examples.bert.src.mosaic_bert import create_mosaic_bert_mlm
 from examples.common.builders import (build_algorithm, build_callback,
                                       build_dataloader, build_logger,
                                       build_optimizer, build_scheduler)
-
-
-def calculate_batch_size_info(global_batch_size, device_microbatch_size):
-    if global_batch_size % dist.get_world_size() != 0:
-        raise ValueError(
-            f'Global batch size {global_batch_size} is not divisible by {dist.get_world_size()} '
-            'as a result, the batch size would be truncated, please adjust `global_batch_size` '
-            f'to be divisible by world size, {dist.get_world_size()}.')
-    device_batch_size = global_batch_size // dist.get_world_size()
-    if device_microbatch_size == 'auto':
-        device_grad_accum = 'auto'
-    elif isinstance(device_microbatch_size, int):
-        if device_microbatch_size > device_batch_size:
-            print(
-                f'WARNING: device_microbatch_size > device_batch_size, '
-                f'will be reduced from {device_microbatch_size} -> {device_batch_size}.'
-            )
-            device_microbatch_size = device_batch_size
-        device_grad_accum = device_batch_size // device_microbatch_size
-    else:
-        raise ValueError(f'Not sure how to parse {device_microbatch_size=}')
-
-    return device_batch_size, device_microbatch_size, device_grad_accum
-
-
-# Coming soon: this conversion math will be done inside Composer Trainer
-def update_batch_size_info(cfg):
-    device_train_batch_size, device_train_microbatch_size, device_train_grad_accum = calculate_batch_size_info(
-        cfg.global_train_batch_size, cfg.device_train_microbatch_size)
-    cfg.n_gpus = dist.get_world_size()
-    cfg.device_train_batch_size = device_train_batch_size
-    cfg.device_train_microbatch_size = device_train_microbatch_size
-    cfg.device_train_grad_accum = device_train_grad_accum
-    # Safely set `device_eval_batch_size` if not provided by user
-    if 'device_eval_batch_size' not in cfg:
-        if cfg.device_train_microbatch_size == 'auto':
-            cfg.device_eval_batch_size = 1  # TODO debug auto eval microbatching
-        else:
-            cfg.device_eval_batch_size = cfg.device_train_microbatch_size
-    return cfg
+from examples.common.config_utils import update_batch_size_info
 
 
 def build_model(cfg: DictConfig):
@@ -92,18 +53,6 @@ def main(cfg: DictConfig,
     model = build_model(cfg.model)
     n_params = sum(p.numel() for p in model.parameters())
     print(f'{n_params=:.4e}')
-
-    # Get batch size info
-    if cfg.global_train_batch_size % dist.get_world_size() != 0:
-        raise ValueError(
-            f'Global batch size {cfg.global_train_batch_size} is not divisible by {dist.get_world_size()} '
-            'as a result, the batch size would be truncated, please adjust `global_train_batch_size` '
-            f'to be divisible by world size, {dist.get_world_size()}.')
-    device_train_batch_size = cfg.global_train_batch_size // dist.get_world_size(
-    )
-    device_eval_batch_size = cfg.get(
-        'global_eval_batch_size',
-        cfg.global_train_batch_size) // dist.get_world_size()
 
     # Dataloaders
     print('Building train loader...')
@@ -179,8 +128,8 @@ def main(cfg: DictConfig,
     config_dict.update({
         'n_gpus': dist.get_world_size(),
         'n_params': n_params,
-        'device_train_batch_size': device_train_batch_size,
-        'device_eval_batch_size': device_eval_batch_size,
+        'device_train_batch_size': cfg.device_train_batch_size,
+        'device_eval_batch_size': cfg.device_eval_batch_size,
     })
     if wandb.run is not None:
         wandb.config.update(config_dict)
