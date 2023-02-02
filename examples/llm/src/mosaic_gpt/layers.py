a1fa0e9c36013913f0a359575ec9ad2b2fe2f962
Abhinav Venigalla <abhi@mosaicml.com>
2023-02-02 21:43:35 +0000

    wip

---
+++ examples/llm/src/mosaic_gpt/layers.py
@@ -0,0 +1,239 @@
+# Copyright 2022 MosaicML Examples authors
+# SPDX-License-Identifier: Apache-2.0
+
+import warnings
+from typing import Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from composer.metrics.nlp import LanguageCrossEntropy, Perplexity
+from composer.models.base import ComposerModel
+from omegaconf import DictConfig
+
+
+class TorchCausalAttention(nn.Module):
+
+    def __init__(self, cfg: DictConfig, device: Optional[str] = None):
+        super().__init__()
+        self.mhsa = nn.MultiheadAttention(
+            embed_dim=cfg.d_model,
+            num_heads=cfg.n_heads,
+            dropout=cfg.attn_pdrop,
+            bias=True,
+            batch_first=True,
+            device=device,
+        )
+        self.mhsa.out_proj._is_residual = True  # type: ignore
+
+        warnings.warn(
+            DeprecationWarning(
+                'Using `attn_impl: torch` is deprecated; recommened using `attn_impl: triton`.'
+            ))
+
+    def forward(self, x, key_padding_mask, attn_mask=None):
+        return self.mhsa(x,
+                         x,
+                         x,
+                         attn_mask=attn_mask,
+                         key_padding_mask=~key_padding_mask,
+                         need_weights=True)
+
+    @staticmethod
+    def mask_shape(n_heads, seq_len, alibi):
+        if alibi:
+            return (n_heads, seq_len, seq_len)
+        return (seq_len, seq_len)
+
+    @staticmethod
+    def attn_mask_(attn_mask, n_heads, seq_len, alibi=False, alibi_bias_max=8):
+        # in-place fill causal attn mask
+        #
+        # Two important disclaimers
+        # 1. Torch uses additive attention. If your attn_mask/key_padding mask is a float tensor, it will add the floats
+        #   directly to your attention matrix. If they are boolean masks, True will be converted to -inf before adding the
+        #   mask to your attentions. See https://pytorch.org/docs/stable/generated/torch.nn.MultiheadAttention.html#torch.nn.MultiheadAttention.forward
+        #   Basically True/-inf indicates tokens we do not want to attend to.
+        #
+        # 2. This is is the exact opposite behavior of Huggingface's tokenizers, which use the convention that True denotes tokens
+        #   we do want to attend to. See https://huggingface.co/docs/transformers/glossary#attention-mask
+        attn_mask.fill_(float('-inf'))
+        attn_mask.triu_(diagonal=1)
+
+        if alibi:
+            device, dtype = attn_mask.device, attn_mask.dtype
+            a_bias = alibi_bias(n_heads,
+                                seq_len,
+                                full=True,
+                                alibi_bias_max=alibi_bias_max,
+                                device=device,
+                                dtype=dtype)
+            attn_mask.add_(a_bias.squeeze())
+
+        return attn_mask
+
+
+class FlashCausalAttention(nn.Module):
+
+    def __init__(self, cfg: DictConfig, device: Optional[str] = None):
+        super().__init__()
+        try:
+            from flash_attn.flash_attention import FlashMHA  # type: ignore
+        except ImportError as e:
+            raise e
+
+        self.mhsa = FlashMHA(
+            embed_dim=cfg.d_model,
+            num_heads=cfg.n_heads,
+            attention_dropout=cfg.attn_pdrop,
+            bias=True,
+            batch_first=True,
+            causal=True,
+            device=device,
+        )
+        self.mhsa.out_proj._is_residual = True
+
+        warnings.warn(
+            DeprecationWarning(
+                'Using `attn_impl: flash` is deprecated; recommened using `attn_impl: triton`.'
+            ))
+
+    def forward(self, x, key_padding_mask, attn_mask=None):
+        assert attn_mask is None
+        return self.mhsa(x,
+                         key_padding_mask=key_padding_mask,
+                         need_weights=False)
+
+    @staticmethod
+    def mask_shape(*args, **kwargs):
+        return None
+
+    @staticmethod
+    def attn_mask_(*args, **kwargs):
+        return None
+
+
+class TritonFlashCausalAttention(nn.Module):
+    """Multi-headed self attention using triton FlashAttn kernel.
+
+    This also includes bias for Alibi integration.
+    """
+
+    def __init__(self, cfg: DictConfig, device: Optional[str] = None):
+        super().__init__()
+        try:
+            from examples.llm.src.flash_attention import \
+                FlashMHA  # type: ignore
+        except ImportError as e:
+            raise e
+
+        assert cfg.attn_pdrop == 0, 'triton kernel does not support attn_dropout'
+
+        self.mhsa = FlashMHA(
+            embed_dim=cfg.d_model,
+            num_heads=cfg.n_heads,
+            bias=True,
+            batch_first=True,
+            causal=True,
+            device=device,
+        )
+        self.mhsa.out_proj._is_residual = True  # type: ignore
+
+    def forward(self, x, key_padding_mask=None, attn_mask=None):
+        assert key_padding_mask is None
+        return self.mhsa(x,
+                         key_padding_mask=None,
+                         attn_mask=attn_mask,
+                         need_weights=False)
+
+    @staticmethod
+    def mask_shape(n_heads, seq_len, alibi):
+        return (1, n_heads, 1, seq_len) if alibi else (1, 1, 1, seq_len)
+
+    @staticmethod
+    def attn_mask_(attn_mask, n_heads, seq_len, alibi=False, alibi_bias_max=8):
+        # in-place fill causal attn mask
+        attn_mask.zero_()
+
+        if alibi:
+            device, dtype = attn_mask.device, attn_mask.dtype
+            attn_mask.add_(
+                alibi_bias(n_heads,
+                           seq_len,
+                           full=False,
+                           alibi_bias_max=alibi_bias_max,
+                           device=device,
+                           dtype=dtype))
+
+        return attn_mask
+
+
+def alibi_bias(n_heads,
+               seq_len,
+               full=False,
+               alibi_bias_max=8,
+               device=None,
+               dtype=None):
+    alibi_bias = torch.arange(1 - seq_len, 1, dtype=dtype,
+                              device=device).view(1, 1, 1, seq_len)
+    if full:
+        # generate 1 x Heads x SeqLen x SeqLen alibi bias mask
+        # otherwise the mask is 1 x Heads x 1 x SeqLen (which is braodcasted up to the approproate size)
+        alibi_bias = alibi_bias - torch.arange(
+            1 - seq_len, 1, dtype=dtype, device=device).view(1, 1, seq_len, 1)
+        alibi_bias.abs_().mul_(
+            -1
+        )  # since we're using causal flag, this isn't really needed, but why not include it
+
+    m = torch.arange(1, n_heads + 1, dtype=dtype, device=device)
+    m.mul_(alibi_bias_max / n_heads)
+    alibi_bias = alibi_bias * (1. / (2**m.view(1, n_heads, 1, 1)))
+    return alibi_bias
+
+
+class GPTMLP(nn.Module):
+
+    def __init__(self, cfg: DictConfig, device: Optional[str] = None):
+        super().__init__()
+        self.mlp_up = nn.Linear(cfg.d_model,
+                                cfg.mlp_ratio * cfg.d_model,
+                                device=device)
+        self.mlp_act = nn.GELU(approximate='none')
+        self.mlp_down = nn.Linear(cfg.mlp_ratio * cfg.d_model,
+                                  cfg.d_model,
+                                  device=device)
+        self.mlp_down._is_residual = True  # type: ignore
+
+    def forward(self, x):
+        return self.mlp_down(self.mlp_act(self.mlp_up(x)))
+
+
+class GPTBlock(nn.Module):
+
+    def __init__(self,
+                 cfg: DictConfig,
+                 causal_attn_cls,
+                 device: Optional[str] = None):
+        super().__init__()
+        if cfg.get('alibi', False):
+            assert cfg.attn_impl == 'triton' or cfg.attn_impl == 'torch', 'Only triton kernel or torch supports alibi'
+        self.ln_1 = nn.LayerNorm(cfg.d_model, device=device)
+        self.causal_attn = causal_attn_cls(cfg, device)
+        self.ln_2 = nn.LayerNorm(cfg.d_model, device=device)
+        self.mlp = GPTMLP(cfg, device=device)
+        self.resid_attn_dropout = nn.Dropout(cfg.resid_pdrop)
+        self.resid_mlp_dropout = nn.Dropout(cfg.resid_pdrop)
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        key_padding_mask: Optional[torch.ByteTensor] = None,
+        attn_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        a = self.ln_1(x)
+        b, _ = self.causal_attn(a, key_padding_mask, attn_mask)
+        x = x + self.resid_attn_dropout(b)
+        m = self.ln_2(x)
+        n = self.mlp(m)
+        x = x + self.resid_mlp_dropout(n)
+        return x
