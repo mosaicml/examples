1ee7edb8e7ad44d28ba7f2ffe02fdf5e500d8b92
Abhinav Venigalla <abhi@mosaicml.com>
2023-02-08 21:54:30 +0000

    fix again

--- examples/llm/src/hf_causal_lm.py
+++ examples/llm/src/hf_causal_lm.py
@@ -1,15 +1,17 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import Optional
+
 import torch
 import torch.nn.functional as F
-from composer.metrics.nlp import (HFCrossEntropy, InContextLearningMetric,
-                                  LanguageCrossEntropy, Perplexity)
+from composer.metrics.nlp import (InContextLearningMetric, LanguageCrossEntropy,
+                                  Perplexity)
 from composer.models.huggingface import HuggingFaceModel
 from omegaconf import DictConfig
+from torch import Tensor
 from torchmetrics import Metric
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
-from transformers.modeling_outputs import CausalLMOutput
 
 from examples.common.hf_fsdp import prepare_hf_causal_lm_model_for_fsdp
 
@@ -20,14 +22,13 @@ class ComposerHFCausalLM(HuggingFaceModel):
         config = AutoConfig.from_pretrained(cfg.pretrained_model_name_or_path)
         tokenizer = AutoTokenizer.from_pretrained(
             cfg.pretrained_model_name_or_path)
+        metrics = [LanguageCrossEntropy(len(tokenizer)), Perplexity()]
 
         if cfg.pretrained:
             model = AutoModelForCausalLM.from_pretrained(
                 cfg.pretrained_model_name_or_path, config=config)
-            metrics = [HFCrossEntropy(), Perplexity()]
         else:
             model = AutoModelForCausalLM.from_config(config)
-            metrics = [LanguageCrossEntropy(len(tokenizer)), Perplexity()]
 
         prepare_hf_causal_lm_model_for_fsdp(model)
 
@@ -41,24 +42,25 @@ class ComposerHFCausalLM(HuggingFaceModel):
         targets[:, -1] = -100
         return targets
 
-    def forward(self, batch: dict) -> CausalLMOutput:
+    def forward(self, batch: dict):
         return self.model(input_ids=batch['input_ids'],
-                          attention_mask=batch['attention_mask'].bool())
+                          attention_mask=batch['attention_mask'].bool()).logits
 
-    def loss(self, outputs: CausalLMOutput, batch: dict):
-        logits = outputs.logits
+    def eval_forward(self, batch: dict, outputs: Optional[Tensor] = None):
+        return outputs if outputs is not None else self.forward(batch)
+
+    def loss(self, outputs: Tensor, batch: dict):
         targets = self.get_targets(batch)
-        return F.cross_entropy(logits.view(-1, logits.size(-1)),
+        return F.cross_entropy(outputs.view(-1, outputs.size(-1)),
                                targets.view(-1),
                                ignore_index=-100)
 
-    def update_metric(self, batch: dict, outputs: CausalLMOutput,
-                      metric: Metric) -> None:
-        logits = outputs.logits.view(-1, outputs.size(-1))
+    def update_metric(self, batch: dict, outputs: Tensor, metric: Metric):
+        outputs = outputs.view(-1, outputs.size(-1))
         targets = self.get_targets(batch).view(-1)
         if isinstance(metric, InContextLearningMetric) and batch.get(
                 'mode', None) == 'icl_task':
-            metric.update(batch, logits, targets)
+            metric.update(batch, outputs, targets)
         else:
-            metric.update(logits,
+            metric.update(outputs,
                           targets)  # pyright: ignore [reportGeneralTypeIssues]
