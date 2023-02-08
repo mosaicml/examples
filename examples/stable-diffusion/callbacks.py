5c1b6d6010c30978c5a261eb794599aaed94f730
austin <jacobsonaustinj@gmail.com>
2023-02-07 19:11:38 -0800

    update ci yamls, remove default xformers

--- examples/stable-diffusion/callbacks.py
+++ examples/stable-diffusion/callbacks.py
@@ -23,33 +23,39 @@ class LogDiffusionImages(Callback):
         num_images_per_prompt = state.model.module.num_images_per_prompt
         for destination in ensure_tuple(logger.destinations):
             if isinstance(destination, WandBLogger):
+                if num_images_per_prompt > 1: 
+                    outputs = [make_grid(out, nrow=num_images_per_prompt) for out in outputs.chunk(num_images_per_prompt)]
+                    
+                for prompt, output in zip(prompts, outputs):
+                    destination.log_images(images=output, name=prompt, step=state.timestamp.batch.value)
+                    
                 # 1 prompt 1 image
-                if len(prompts) == 1 and num_images_per_prompt == 1:
-                    destination.log_images(images=output[0],
-                                           name=prompt[0],
-                                           step=state.timestamp.batch.value)
+                # if len(prompts) == 1 and num_images_per_prompt == 1:
+                #     destination.log_images(images=output[0],
+                #                            name=prompt[0],
+                #                            step=state.timestamp.batch.value)
 
-                # multiple prompts, 1 image per prompt
-                if len(prompts) > 1 and num_images_per_prompt == 1:
-                    for prompt, output in zip(prompts, outputs):
-                        destination.log_images(images=output,
-                                               name=prompt,
-                                               step=state.timestamp.batch.value)
+                # # multiple prompts, 1 image per prompt
+                # if len(prompts) > 1 and num_images_per_prompt == 1:
+                #     for prompt, output in zip(prompts, outputs):
+                #         destination.log_images(images=output,
+                #                                name=prompt,
+                #                                step=state.timestamp.batch.value)
 
-                # one prompt multiple images
-                if len(prompts) == 1 and num_images_per_prompt > 1:
-                    outputs = make_grid(outputs, nrow=num_images_per_prompt)
-                    destination.log_images(images=outputs,
-                                           name=prompts[0],
-                                           step=state.timestamp.batch.value)
+                # # one prompt multiple images
+                # if len(prompts) == 1 and num_images_per_prompt > 1:
+                #     outputs = make_grid(outputs, nrow=num_images_per_prompt)
+                #     destination.log_images(images=outputs,
+                #                            name=prompts[0],
+                #                            step=state.timestamp.batch.value)
 
-                # multiple prompts multiple images
-                if len(prompts) > 1 and num_images_per_prompt > 1:
-                    outputs = [
-                        make_grid(out, nrow=num_images_per_prompt)
-                        for out in outputs.chunk(num_images_per_prompt)
-                    ]
-                    for prompt, output in zip(prompts, outputs):
-                        destination.log_images(images=outputs,
-                                               name=prompt,
-                                               step=state.timestamp.batch.value)
+                # # multiple prompts multiple images
+                # if len(prompts) > 1 and num_images_per_prompt > 1:
+                #     outputs = [
+                #         make_grid(out, nrow=num_images_per_prompt)
+                #         for out in outputs.chunk(num_images_per_prompt)
+                #     ]
+                #     for prompt, output in zip(prompts, outputs):
+                #         destination.log_images(images=outputs,
+                #                                name=prompt,
+                #                                step=state.timestamp.batch.value)
