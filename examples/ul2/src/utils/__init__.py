a1727c3dd69b0b155137709c9db05c92456bf305
Alex Trott <alex@mosaicml.com>
2023-02-10 19:17:27 -0800

    +1 file to last commit

--- examples/ul2/src/utils/__init__.py
+++ examples/ul2/src/utils/__init__.py
@@ -3,11 +3,11 @@
 
 from examples.ul2.src.utils.adapt_tokenizer import (
     AutoTokenizerForMOD, adapt_tokenizer_for_denoising)
+from examples.ul2.src.utils.finetuning import Seq2SeqFinetuningCollator
 from examples.ul2.src.utils.hf_prefixlm_converter import \
     convert_hf_causal_lm_to_prefix_lm
 
 __all__ = [
-    'AutoTokenizerForMOD',
-    'adapt_tokenizer_for_denoising',
-    'convert_hf_causal_lm_to_prefix_lm',
+    'AutoTokenizerForMOD', 'adapt_tokenizer_for_denoising',
+    'convert_hf_causal_lm_to_prefix_lm', 'Seq2SeqFinetuningCollator'
 ]
