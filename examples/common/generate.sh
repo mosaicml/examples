4b576297fb4b2ad6da26da85dd61bd70708aba43
Abhinav Venigalla <abhi@mosaicml.com>
2023-02-11 00:49:32 +0000

    remove unneeded files

--- examples/common/generate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ROOT=/home/mosaicml/gpt2_tokenized_2k; python convert_c4.py --out_root $ROOT --splits train train_small val val_small --concat_tokens=2048 --tokenizer=gpt2 --eos_text="<|endoftext|>" --compression zstd
--- examples/llm/fast-opt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source venv-llm/bin/activate
-composer train_clm/train_clm.py train_clm/yamls/opt/125m.yaml \
-    train_loader.dataset.split=train_small \
-    # data_local=/tmp/the-pile \
-    # data_remote=s3://mosaicml-internal-dataset-the-pile/mds/2 \
-    eval_interval=0 \
-    max_duration=3ba \
-    # save_interval=1ba save_folder="./opt-125m/"
