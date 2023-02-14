f59ad1eb14d17759c12ce633dabbac0d19c493da
Abhinav Venigalla <abhi@mosaicml.com>
2023-02-14 08:28:05 -0800

    typo

--- examples/llm/throughput/sweep.sh
+++ examples/llm/throughput/sweep.sh
@@ -9,7 +9,7 @@ CLUSTER_80GB=YOUR_CLUSTER_80GB
 # python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 760m.yaml -g 8 16 32 64 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 6
 # python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 1b.yaml -g 8 16 32 64 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 4
 # python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 3b.yaml -g 8 16 32 64 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 14
-# python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 7b.yaml -g 8 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 8
+# python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 7b.yaml -g 8 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 6
 # python run_all_configs.py --project $PROJECT -s 11 11 -b 19 22 -m 7b.yaml -g 16 32 64 --image mosaicml/examples:llm-latest --git_commit $GIT_COMMIT --pad_vocab_multiple 16 --gpu_type a100_40gb --cluster $CLUSTER_40GB --RUN --microbatch_size 8
 
 
