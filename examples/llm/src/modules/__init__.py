9e40a885640d6718b90b6b94c8aa134fc7d75838
brandon <bcui8377@gmail.com>
2023-02-21 19:41:11 +0000

    fixing init

--- examples/llm/src/modules/__init__.py
+++ examples/llm/src/modules/__init__.py
@@ -1,6 +1,12 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
+from examples.llm.src.modules.gpt_blocks import GPTMLP, GPTBlock
+from examples.llm.src.modules.attention_modules import (TorchCausalAttention,
+                                     FlashCausalAttention, TritonFlashCausalAttention
+)
+
+
 __all__ = [
     'TorchCausalAttention',
     'FlashCausalAttention',
