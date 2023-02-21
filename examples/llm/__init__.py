879ba35e4881d585401efa149125967b3a09a862
brandon <bcui8377@gmail.com>
2023-02-21 18:37:19 +0000

    Making modules folder for gpt

--- examples/llm/__init__.py
+++ examples/llm/__init__.py
@@ -7,11 +7,11 @@ try:
     from examples.llm.src.flash_attention import FlashAttention, FlashMHA
     from examples.llm.src.hf_causal_lm import ComposerHFCausalLM
     from examples.llm.src.model_registry import COMPOSER_MODEL_REGISTRY
-    from examples.llm.src.mosaic_gpt import (GPTMLP, ComposerMosaicGPT,
-                                             FlashCausalAttention, GPTBlock,
-                                             MosaicGPT, TorchCausalAttention,
-                                             TritonFlashCausalAttention,
-                                             alibi_bias)
+    from examples.llm.src.modules.attention_modules import (
+        FlashCausalAttention, TorchCausalAttention, TritonFlashCausalAttention,
+        alibi_bias)
+    from examples.llm.src.modules.gpt_blocks import GPTMLP, GPTBlock
+    from examples.llm.src.mosaic_gpt import ComposerMosaicGPT, MosaicGPT
     from examples.llm.src.tokenizer import (TOKENIZER_REGISTRY, HFTokenizer,
                                             LLMTokenizer)
 except ImportError as e:
---
+++ examples/llm/src/modules/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2022 MosaicML Examples authors
+# SPDX-License-Identifier: Apache-2.0
+
+
+__all__ = [
+    'TorchCausalAttention',
+    'FlashCausalAttention',
+    'TritonFlashCausalAttention',
+    'GPTMLP',
+    'GPTBlock',
+]
