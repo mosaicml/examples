18387dee335075d8c9be8ee431bef2d06bd4af53
Daniel King <43149077+dakinggg@users.noreply.github.com>
2023-03-02 11:46:40 -0800

    replace speed monitor with composer one (#207)

--- examples/llm/src/models/mosaic_gpt.py
+++ examples/llm/src/models/mosaic_gpt.py
@@ -244,7 +244,7 @@ class ComposerMosaicGPT(ComposerModel):
     def __init__(self, cfg):
         super().__init__()
         self.model = MosaicGPT(cfg)
-        self.__num_fwd_flops = None
+        self.num_fwd_flops = self._compute_num_fwd_flops()
         self.train_metrics = {
             'LanguageCrossEntropy': LanguageCrossEntropy(cfg.vocab_size),
             'Perplexity': Perplexity(),
@@ -299,10 +299,7 @@ class ComposerMosaicGPT(ComposerModel):
         else:
             self.eval_metrics = evaluator_metrics
 
-    @property
-    def num_fwd_flops(self):
-        if self.__num_fwd_flops:
-            return self.__num_fwd_flops
+    def _compute_num_fwd_flops(self):
         n_params = sum(p.numel() for p in self.parameters())
         # the number of paramters is approximately the number of multiply-accumulates (MAC) in the network
         # each MAC has 2 FLOPs - we multiply by 2 ie 2 * n_param
@@ -312,5 +309,10 @@ class ComposerMosaicGPT(ComposerModel):
         # there are 2 FLOPS per mac; there is A=Q*K^T and out=A*V ops (ie mult by 2)
         attn_flops_per_seq = self.model.cfg.n_layers * 2 * 2 * (
             self.model.cfg.d_model * (self.model.cfg.max_seq_len**2))
-        self.__num_fwd_flops = params_flops_per_seq + attn_flops_per_seq
-        return self.__num_fwd_flops
+        return params_flops_per_seq + attn_flops_per_seq
+
+    def flops_per_batch(self, batch):
+        # Note: this computation does not take into account padding, and assumes
+        # that the dataset has been constructed without padding. Additionally, we 
+        # assume the backward pass is approximately 2x the forward pass
+        return self.num_fwd_flops * 3 * batch['input_ids'].shape[0]
