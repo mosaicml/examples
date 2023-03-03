f00d1249af7f30bee2ee907515eeb63658df0660
nik-mosaic <None>
2023-03-03 17:46:25 +0000

    Run precommit

--- examples/llm/src/models/ops/__init__.py
+++ examples/llm/src/models/ops/__init__.py
@@ -1,9 +1,10 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
-from examples.llm.src.models.ops.layer_norm import DropoutAddLayerNorm
-from examples.llm.src.models.ops.cross_entropy import CrossEntropyLoss, check_if_xentropy_cuda_installed
+from examples.llm.src.models.ops.cross_entropy import (
+    CrossEntropyLoss, check_if_xentropy_cuda_installed)
 from examples.llm.src.models.ops.fused_dense import DenseResGeluDense
+from examples.llm.src.models.ops.layer_norm import DropoutAddLayerNorm
 
 __all__ = [
     'CrossEntropyLoss',
