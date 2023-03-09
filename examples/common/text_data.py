5a54d0feb842b181143ac54dc3f424e3ca27d826
Matthew Leavitt <matthew@mosaicml.com>
2023-03-09 02:49:08 +0000

    need to fix super().__init in StreamingTextDataset

--- examples/common/text_data.py
+++ examples/common/text_data.py
@@ -5,14 +5,14 @@
 
 import os
 from itertools import islice
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Sequence
 
 import numpy as np
 import torch
 import transformers
 from omegaconf import DictConfig
 from omegaconf import OmegaConf as om
-from streaming import StreamingDataset
+from streaming import StreamingDataset, Stream
 from torch.utils.data import DataLoader
 
 
@@ -20,7 +20,16 @@ class StreamingTextDataset(StreamingDataset):
     """Generic text dataset using MosaicML's StreamingDataset.
 
     Args:
-        local (str): Local dataset directory where shards are cached by split.
+        streams (Sequence[Stream], optional): One or more Streams to stream/cache samples
+            from, which may be upsampled or downsampled. StreamingDataset uses either
+            ``streams`` or ``remote``/``local``. Defaults to ``None``.
+        remote (str, optional): Remote path or directory to download the dataset from. If
+            ``None``, its data must exist locally. StreamingDataset uses either
+            ``streams`` or ``remote``/``local``. Defaults to ``None``.
+        local (str, optional): Local working directory to download shards to. This is
+            where shards are cached while they are being used. Uses a temp directory if
+            not set. StreamingDataset uses either ``streams`` or ``remote``/``local``.
+            Defaults to ``None``.
         tokenizer_name (str): The name of the HuggingFace tokenizer to use to
             tokenize samples.
         max_seq_len (int): The max sequence length of each sample.
@@ -46,14 +55,15 @@ class StreamingTextDataset(StreamingDataset):
     """
 
     def __init__(self,
-                 local: str,
                  tokenizer_name: str,
                  max_seq_len: int,
+                 streams: Optional[Sequence[Stream]] = None,
+                 local: Optional[str] = None,
                  remote: Optional[str] = None,
                  split: Optional[str] = None,
                  shuffle: bool = False,
                  predownload: Optional[int] = 100_000,
-                 keep_zip: Optional[bool] = None,
+                 keep_zip: bool = False,
                  download_retry: int = 2,
                  download_timeout: float = 60,
                  validate_hash: Optional[str] = None,
@@ -74,16 +84,18 @@ class StreamingTextDataset(StreamingDataset):
                 f'StreamingTextDataset() got an unexpected keyword argument: {kwargs}'
             )
 
-        if remote is None or (local == remote):
-            if os.path.isdir(local):
-                contents = set(os.listdir(local))
-                if split not in contents:
-                    raise ValueError(
-                        f'local directory {local} does not contain split {split}'
-                    )
+        if remote is None or (local == remote) or streams is None:
+            if local is not None:
+                if os.path.isdir(local):
+                    contents = set(os.listdir(local))
+                    if split not in contents:
+                        raise ValueError(
+                            f'local directory {local} does not contain split {split}'
+                        )
 
         # Build Dataset
-        super().__init__(local=local,
+        super().__init__(streams=streams,
+                         local=local,
                          remote=remote,
                          split=split,
                          shuffle=shuffle,
@@ -136,6 +148,15 @@ class StreamingTextDataset(StreamingDataset):
             )
         return token_sample
 
+def build_streams(cfg: DictConfig):
+    if cfg.get('streams', None) is not None:
+        streams = []
+        for stream_name in cfg.streams:
+            streams.append(Stream(**cfg.streams.get(stream_name)))
+        return streams
+    else:
+        return None
+
 
 def build_text_dataloader(cfg: DictConfig, device_batch_size: int):
     assert cfg.name == 'text', f'Tried to build text dataloader with cfg.name={cfg.name}'
@@ -144,8 +165,10 @@ def build_text_dataloader(cfg: DictConfig, device_batch_size: int):
             'group_method is deprecated and has been removed.\nTo ' +
             'concatenate, use the --concat_tokens ' +
             'argument when creating your MDS dataset with concat_c4.py')
+    streams = build_streams(cfg.dataset)
     dataset = StreamingTextDataset(
-        local=cfg.dataset.local,
+        streams=streams,
+        local=cfg.dataset.get('local', None),
         tokenizer_name=cfg.dataset.tokenizer_name,
         max_seq_len=cfg.dataset.max_seq_len,
         remote=cfg.dataset.get('remote', None),
