e5286bd3ecf7a31bdad7d178c6a5e84f4fc148d7
Daniel King <43149077+dakinggg@users.noreply.github.com>
2023-03-22 11:50:28 -0700

    Update examples/llm/src/models/layers/attention.py
    Co-authored-by: Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>

--- examples/llm/src/models/layers/attention.py
+++ examples/llm/src/models/layers/attention.py
@@ -17,7 +17,7 @@ from torch import nn
 
 def _reset_is_causal(num_query_tokens: int, num_key_tokens: int,
                      original_is_causal: bool):
-    if num_query_tokens != num_key_tokens:
+    if original_is_causal and num_query_tokens != num_key_tokens:
         if num_query_tokens != 1:
             raise NotImplementedError(
                 'MosaicGPT does not support query and key with different number of tokens, unless number of query tokens is 1.'
