b40c1a9802700eb9382cc0697ed64f79ca9d6fd1
nik-mosaic <None>
2023-03-23 23:56:35 +0000

    i Add fused xentropy test

--- examples/llm/src/models/mosaic_gpt/mosaic_gpt.py
+++ examples/llm/src/models/mosaic_gpt/mosaic_gpt.py
@@ -309,8 +309,7 @@ class ComposerMosaicGPT(ComposerModel):
             'Perplexity':
                 Perplexity(),
         }
-        self.loss_fn = resolved_om_config.get('loss_fn', 'fused_crossentropy')
-        if self.loss_fn == 'fused_crossentropy':
+        if hf_config.loss_fn == 'fused_crossentropy':
             try:
                 from flash_attn.losses.cross_entropy import CrossEntropyLoss as FusedCrossEntropyLoss  # type: ignore # isort: skip
                 print('Using Fused Cross Entropy Loss.')
