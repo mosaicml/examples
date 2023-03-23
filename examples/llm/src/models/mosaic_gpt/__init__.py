60bb3235aa7ffbdca333b8a6fa376860608028b9
Vitaliy Chiley <vitaliy@mosaicml.com>
2023-03-23 17:11:32 +0000

    reorg model file struct

---
+++ examples/llm/src/models/mosaic_gpt/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2022 MosaicML Examples authors
+# SPDX-License-Identifier: Apache-2.0
+
+from examples.llm.src.models.mosaic_gpt.mosaic_gpt import (ComposerMosaicGPT,
+                                                           MosaicGPT)
+
+__all__ = [
+    'MosaicGPT',
+    'ComposerMosaicGPT',
+]
--- examples/llm/src/models/mosaic_gpt/mosaic_gpt.py
+++ examples/llm/src/models/mosaic_gpt/mosaic_gpt.py
@@ -25,7 +25,7 @@ from transformers.modeling_outputs import CausalLMOutputWithPast
 
 import examples.llm.src.models.layers.attention as attention
 import examples.llm.src.models.layers.gpt_blocks as gpt_blocks
-from examples.llm.src.models.configuration_mosaic_gpt import MosaicGPTConfig
+from examples.llm.src.models.mosaic_gpt.configuration_mosaic_gpt import MosaicGPTConfig
 from examples.llm.src.models.utils.param_init_fns import MODEL_INIT_REGISTRY
 
 
