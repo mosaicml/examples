db5975d15de7c07bb90d24c233429243ddaa1c9c
Vitaliy Chiley <vitaliy@mosaicml.com>
2023-03-24 02:10:04 +0000

    lint

--- examples/llm/src/models/utils/__init__.py
+++ examples/llm/src/models/utils/__init__.py
@@ -6,10 +6,11 @@ from examples.llm.src.models.utils.adapt_tokenizer import (
 from examples.llm.src.models.utils.hf_prefixlm_converter import \
     convert_hf_causal_lm_to_prefix_lm
 from examples.llm.src.models.utils.meta_init_context import init_empty_weights
-from examples.llm.src.models.utils.param_init_fns import MODEL_INIT_REGISTRY
+from examples.llm.src.models.utils.param_init_fns import (  # type: ignore
+    MODEL_INIT_REGISTRY, generic_param_init_fn_)
 
 __all__ = [
     'AutoTokenizerForMOD', 'adapt_tokenizer_for_denoising',
     'convert_hf_causal_lm_to_prefix_lm', 'init_empty_weights',
-    'MODEL_INIT_REGISTRY'
+    'generic_param_init_fn_', 'MODEL_INIT_REGISTRY'
 ]
