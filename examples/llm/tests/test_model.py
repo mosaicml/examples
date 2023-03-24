2a118b7b1894f6a907cb4b37753e74dcb0c5629b
nik-mosaic <None>
2023-03-24 00:01:07 +0000

    reduce num_batches in test

--- examples/llm/tests/test_model.py
+++ examples/llm/tests/test_model.py
@@ -354,7 +354,7 @@ def test_loss_fn():
                                  eps=test_cfg.optimizer.eps,
                                  weight_decay=test_cfg.optimizer.weight_decay)
 
-    for i in range(100):
+    for i in range(25):
         batch = gen_random_batch(2, test_cfg)
         output_1 = model_1(batch)
         output_2 = model_2(batch)
