3ea70c03c4ac0e205d2a38300eb801b91fa7c55a
bmosaicml <jeremy@mosaicml.com>
2023-03-28 13:38:25 -0400

    add docs

--- examples/common/optim/outlier_detection.py
+++ examples/common/optim/outlier_detection.py
@@ -2,18 +2,28 @@
 import collections
 
 class OutlierDetector:
-    
+    """This class implements an algorithm to detect outliers in sequential numeric data (e.g. for gradient/moment norms in optimizers). It relies on a delayed moving average
+    which is the moving average of observations from time step T-2*`delay_interval` to T-`delay_interval`. The motivation is that outliers typically occur in clusters 
+    that can potentially stretch for many observations, hence it's best to use a delayed moving average to detect outliers.
+
+    It defines an outlier as any data point that is `threshold` times larger than the delayed moving average.
+
+    The class assumes data is inserted sequentially at evenly spaced intervals of unit length 1.
+    """
     def __init__(self,
                  threshold: float = 7.5,
-                 increase_lookback: int = 500):
+                 delay_interval: int = 500):
           
-        self.intermediate_data_queue = collections.deque(maxlen=increase_lookback)
-        self.slow_moving_average = collections.deque(maxlen=increase_lookback)
-        self.outlier_threshold = threshold
+        self.intermediate_data_queue = collections.deque(maxlen=delay_interval)
+        self.delayed_moving_average = collections.deque(maxlen=delay_interval)
+        self.threshold = threshold
     
     def insert_observation(self, obs: float) -> bool:
-        """Inserts obs into the data buffer and returns true if it is an "outlier", defined `outlier_threshold` times larger than
-        the windowed moving average from [T-2*`increase_lookback` : T-`increase_lookback`]
+        """Inserts obs into the data buffer and returns true if it is an "outlier", defined `threshold` times larger than
+        the windowed moving average from [T-2*`delay_interval` : T-`delay_interval`].
+
+        This algorithm first moves recent data into an intermediate circular buffer, and then moves data in to the delayed moving average buffer
+        once it is old enough to be evicted from the intermediate data. This is to ensure that we take a delayed moving average that doesn't include recent data.
 
         Args:
             obs (float): Numeric observation for the current timestep.
@@ -22,20 +32,17 @@ class OutlierDetector:
             bool: Indicator of whether the most recent observation was an outlier.
         """
         if len(self.intermediate_data_queue) >= self.intermediate_data_queue.maxlen:
-                # move data from intermediate queue to slow MCVA queue
+                # move data from intermediate queue to slow moving average queue
                 intermediate_obs = self.intermediate_data_queue.popleft()
-                self.slow_moving_average.append(intermediate_obs)
+                self.delayed_moving_average.append(intermediate_obs)
 
         self.intermediate_data_queue.append(obs)
+        delayed_mva = self.get_delayed_mva()
+        return delayed_mva is not None and obs > self.threshold * delayed_mva
         
-        if len(self.slow_moving_average) > 0:
-            slow_mva = sum(self.slow_moving_average) / len(self.slow_moving_average)
-            return obs > self.outlier_threshold * slow_mva
-        else:
-            return False
     
-    def get_slow_mva(self):
-        if len(self.slow_moving_average) > 0:
-            return sum(self.slow_moving_average) / len(self.slow_moving_average)
+    def get_delayed_mva(self):
+        if len(self.delayed_moving_average) > 0:
+            return sum(self.delayed_moving_average) / len(self.delayed_moving_average)
         else:
-            return 0
+            return None
