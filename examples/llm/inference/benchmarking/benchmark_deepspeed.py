cf771bfa8188ff9593684ddb01c5bd803909f778
brandon <bcui8377@gmail.com>
2023-03-30 18:24:46 +0000

    Adding initial deepspeed benchmarking

---
+++ examples/llm/inference/benchmarking/benchmark_deepspeed.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import time
+import torch
+import transformers
+import deepspeed
+import numpy as np
+from omegaconf import OmegaConf as om
+
+# You can use this to load the model weights
+from composer import Trainer
+
+# from composer.utils import dist, get_device
+from examples.llm.src import COMPOSER_MODEL_REGISTRY
+
+
+def main(config):
+    tokenizer_name = "gpt2"
+
+    # TODO: maybe move this?
+    inf_config = {
+        "replace_with_kernel_inject": True,
+        "dtype": torch.float16,
+        "replace_method": "auto",
+        "enable_cuda_graph": False,
+        "tensor_parallel": {"tp_size": 1},
+    }
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_name)
+
+    model = COMPOSER_MODEL_REGISTRY[config.model.name](config.model, config.tokenizer)
+    model.eval()
+
+    inference_model = deepspeed.init_inference(model, config=inf_config)
+
+    # Checking if deepspeed casts dtypes correctly
+    for n, p in inference_model.named_parameters():
+        print("n is: ", n, p)
+        print ("dtype is: ", p.dtype)
+        break
+    
+    stats = []
+    for batch_size in config.batch_sizes:
+        for input_length in config.input_lengths:
+            for output_length in config.output_lengths:
+                times = []
+                for _ in range(config.num_runs):
+                    start_time = time.time()
+                    batch = torch.ones((batch_size, input_length)).cuda() * 17
+                    batch = batch.to(torch.long)
+                    out = inference_model.generate(batch, max_new_tokens=output_length, use_cache=True)
+                    times.append(time.time() - start_time)
+                
+                num_output_tokens = output_length * batch_size
+                mean_time = np.mean(times)
+                tokens_per_second = num_output_tokens / float(mean_time)
+
+                # TODO: make this an experiment param
+                resu = (f'{config.benchmark_name}_{batch_size}_{input_length}_{output_length}', f"{tokens_per_second:.3f}")
+
+                run_name, tokens_per_second = resu
+
+                print (f"{run_name}, {tokens_per_second}")
+
+                stats.append(resu)
+
+if __name__ == "__main__":
+    yaml_path, args_list = sys.argv[1], sys.argv[2:]
+    with open(yaml_path) as f:
+        yaml_config = om.load(f)
+    cli_config = om.from_cli(args_list)
+    config = om.merge(yaml_config, cli_config)
+    main(config)
