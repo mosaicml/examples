ea89bccb9df7c752b9d8112e390f611df7d534c6
Abhinav Venigalla <abhi@mosaicml.com>
2023-04-06 20:56:22 +0000

    make convert upload to uri

--- examples/llm/inference/convert_composer_to_hf.py
+++ examples/llm/inference/convert_composer_to_hf.py
@@ -1,9 +1,8 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""TBD."""
-
 import json
+import os
 import tempfile
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
@@ -11,26 +10,29 @@ from typing import Any, Dict, Optional, Union
 
 import sentencepiece as spm
 import torch
-from composer.utils import get_file, safe_torch_load
+from composer.utils import (get_file, maybe_create_object_store_from_uri,
+                            parse_uri, safe_torch_load)
 from transformers import (AutoConfig, AutoTokenizer, PretrainedConfig,
                           PreTrainedTokenizer)
 
 from examples.llm import MosaicGPTConfig
 
 
+# TODO: maybe move this functionality to Composer
 def get_hf_config_from_composer_state_dict(
         state_dict: Dict[str, Any]) -> PretrainedConfig:
     hf_config_dict = state_dict['state']['integrations']['huggingface'][
         'model']['config']['content']
 
-    AutoConfig.register('mosaic_gpt', MosaicGPTConfig)
-    hf_config = AutoConfig.for_model(**hf_config_dict)
     # Always set init_device='cpu'
-    hf_config.init_device = 'cpu'
-    return hf_config
+    hf_config_dict['init_device'] = 'cpu'
+
+    AutoConfig.register('mosaic_gpt', MosaicGPTConfig)
+    return AutoConfig.for_model(**hf_config_dict)
 
 
-def get_hf_tokenizer_from_comopser_state_dict(
+# TODO: maybe move this functionality to Composer
+def get_hf_tokenizer_from_composer_state_dict(
         state_dict: Dict[str, Any]) -> Optional[PreTrainedTokenizer]:
     hf_tokenizer_state = state_dict['state']['integrations']['huggingface'][
         'tokenizer']
@@ -64,7 +66,7 @@ def get_hf_tokenizer_from_comopser_state_dict(
 
 def write_huggingface_pretrained_from_composer_checkpoint(
         checkpoint_path: Union[Path, str],
-        output_folder: Union[Path, str],
+        output_path: Union[Path, str],
         local_checkpoint_save_location: Optional[Union[Path,
                                                        str]] = None) -> None:
     """Convert a Composer checkpoint to a pretrained HF checkpoint folder.
@@ -103,7 +105,8 @@ def write_huggingface_pretrained_from_composer_checkpoint(
     Args:
         checkpoint_path (Union[Path, str]): Path to the composer checkpoint, can be a local path, or a remote path beginning with ``s3://``, or another backend
             supported by :meth:`composer.utils.maybe_create_object_store_from_uri`.
-        output_folder (Union[Path, str]): Path to the folder to write the output to. Must be a local path.
+        output_path (Union[Path, str]): Path to the folder to write the output to. Can be a local path, or a remote path beginning with ``s3://``, or another backend
+            supported by :meth:`composer.utils.maybe_create_object_store_from_uri`.
         local_checkpoint_save_location (Optional[Union[Path, str]], optional): If specified, where to save the checkpoint file to locally.
                                                                                 If the input ``checkpoint_path`` is already a local path, this will be a symlink.
                                                                                 Defaults to None, which will use a temporary file.
@@ -114,38 +117,65 @@ def write_huggingface_pretrained_from_composer_checkpoint(
         local_checkpoint_save_location = Path(
             tmp_dir.name) / 'local-composer-checkpoint.pt'
 
+    # create object store if output_path
+    object_store = maybe_create_object_store_from_uri(output_path)
+    if object_store is not None:
+        local_output_path = tempfile.TemporaryDirectory().name
+    else:
+        local_output_path = output_path
+
+    # create folder
+    os.makedirs(local_output_path)
+
     # download the checkpoint file
+    print(
+        f'Downloading checkpoint from {checkpoint_path} -> {local_checkpoint_save_location}'
+    )
     get_file(str(checkpoint_path), str(local_checkpoint_save_location))
 
     # Load the Composer checkpoint state dict
+    print('Loading checkpoint into CPU RAM...')
     composer_state_dict = safe_torch_load(local_checkpoint_save_location)
 
     # Build and save HF Config
+    print('#' * 30)
     print('Saving HF Model Config...')
     hf_config = get_hf_config_from_composer_state_dict(composer_state_dict)
-    hf_config.save_pretrained(output_folder)
+    hf_config.save_pretrained(local_output_path)
     print(hf_config)
 
     # Extract and save the HF tokenizer
     print('#' * 30)
-    print('Saving HF Tokenizer Config...')
-    hf_tokenizer = get_hf_tokenizer_from_comopser_state_dict(
+    print('Saving HF Tokenizer...')
+    hf_tokenizer = get_hf_tokenizer_from_composer_state_dict(
         composer_state_dict)
-    hf_tokenizer.save_pretrained(output_folder)
+    hf_tokenizer.save_pretrained(local_output_path)
     print(hf_tokenizer)
 
-    # # Extract and save the HF model weights
+    # Extract and save the HF model weights
     print('#' * 30)
     print('Saving HF Model Weights...')
-    weights_state_dict = composer_state_dict['state']['model']
-    weights_state_dict = {
-        k: v for k, v in weights_state_dict.items() if k.startswith('model.')
-    }
+    weights_state_dict = composer_state_dict
+    if 'state' in weights_state_dict:
+        weights_state_dict = weights_state_dict['state']['model']
     torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(
         weights_state_dict, prefix='model.')
-    torch.save(weights_state_dict, Path(output_folder) / 'pytorch_model.bin')
+    torch.save(weights_state_dict,
+               Path(local_output_path) / 'pytorch_model.bin')
 
-    print(f'HF checkpoint folder successfully created at {output_folder}.')
+    print('#' * 30)
+    print(f'HF checkpoint folder successfully created at {local_output_path}.')
+
+    if object_store is not None:
+        print(
+            f'Uploading HF checkpoint folder from {local_output_path} -> {output_path}'
+        )
+        for file in os.listdir(local_output_path):
+            _, _, prefix = parse_uri(output_path)
+            remote_file = os.path.join(prefix, file)
+            local_file = os.path.join(local_output_path, file)
+            object_store.upload_object(remote_file, local_file)
+    print('Done.')
     print('#' * 30)
 
 
@@ -155,8 +185,8 @@ def parse_args() -> Namespace:
         description=
         'Convert Composer checkpoint and Omegaconf model config into a standard HuggingFace checkpoint folder.'
     )
-    parser.add_argument('--composer_ckpt', type=str, required=True)
-    parser.add_argument('--hf_folder', type=str, required=True)
+    parser.add_argument('--composer_path', type=str, required=True)
+    parser.add_argument('--hf_path', type=str, required=True)
     parser.add_argument('--local_checkpoint_save_location',
                         type=str,
                         default=None)
@@ -165,14 +195,9 @@ def parse_args() -> Namespace:
 
 
 def main(args: Namespace) -> None:
-    """Main: convert a Composer checkpoint to a HuggingFace pretrained model folder
-
-    Args:
-        args (Namespace): Commandline arguments.
-    """
     write_huggingface_pretrained_from_composer_checkpoint(
-        checkpoint_path=args.composer_ckpt,
-        output_folder=args.hf_folder,
+        checkpoint_path=args.composer_path,
+        output_path=args.hf_path,
         local_checkpoint_save_location=args.local_checkpoint_save_location)
 
 
