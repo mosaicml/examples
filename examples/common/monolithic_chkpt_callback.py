c5eb311c696bd1cabf073358cecf042c303aed9b
Evan Racah <ejracah@gmail.com>
2023-04-06 18:25:44 -0700

    use whole state_dict and pop optimizer

--- examples/common/monolithic_chkpt_callback.py
+++ examples/common/monolithic_chkpt_callback.py
@@ -8,6 +8,7 @@ from composer.core.state import fsdp_state_dict_type_context
 import torch
 import os
 from composer.utils import dist
+import contextlib
 
 
 class MonolithicCheckpointSaver(Callback):
@@ -18,12 +19,13 @@ class MonolithicCheckpointSaver(Callback):
         filename (str): Filename to save checkpoints to.
         batch_interval (int): Number of batches between checkpoints.
     """
-    def __init__(self, save_folder: str, batch_interval: int, filename: str='ep{epoch}-ba{batch}.pt', overwrite: bool = False):
+    def __init__(self, save_folder: str, batch_interval: int, filename: str='ep{epoch}-ba{batch}.pt', overwrite: bool = False, keep_optimizers: bool = False):
         self.backend, self.bucket_name, self.save_dir_format_str = parse_uri(save_folder)
         self.filename_format_str = filename
         self.batch_interval = batch_interval
         self.upload_to_object_store = (self.backend != '')
         self.overwrite = overwrite
+        self.keep_optimizers = keep_optimizers
         if self.upload_to_object_store:
             self.remote_ud = RemoteUploaderDownloader(bucket_uri=f'{self.backend}://{self.bucket_name}')
         else:
@@ -42,27 +44,22 @@ class MonolithicCheckpointSaver(Callback):
         if state.timestamp.batch.value % self.batch_interval == 0:
             filename = format_name_with_dist_and_time(self.filename_format_str, state.run_name, state.timestamp)
             save_dir = format_name_with_dist_and_time(self.save_dir_format_str, state.run_name, state.timestamp)
-            if self.upload_to_object_store:
-                with tempfile.TemporaryDirectory() as tempdir:
-                    temp_save_path = str(Path(tempdir) / Path(filename))
-                    dirname = os.path.dirname(temp_save_path)
-                    if dirname:
-                        os.makedirs(dirname, exist_ok=True)
-                    with fsdp_state_dict_type_context(state.model, state_dict_type='full'):
-                        state_dict = {'state': {'model': state.model.state_dict()}, 'rng': reproducibility.get_rng_state()}
-                        torch.save(state_dict, temp_save_path)
-                    remote_file_name = str(Path(save_dir) / Path(filename))
-                    if dist.get_global_rank() == 0:
-                        self.remote_ud.upload_file(state=state, remote_file_name=remote_file_name, file_path=temp_save_path, overwrite=self.overwrite)
-            else:
-                save_path = str(Path(save_dir) / Path(filename))
+            dir_context_mgr = tempfile.TemporaryDirectory() if self.upload_to_object_store else contextlib.nullcontext(enter_result=save_dir)
+            with dir_context_mgr as temp_save_dir:
+                save_path = str(Path(temp_save_dir) / Path(filename))
                 dirname = os.path.dirname(save_path)
                 if dirname:
                     os.makedirs(dirname, exist_ok=True)
+                state_dict = {'state': state.state_dict(), 'rng': reproducibility.get_rng_state()}
+                if not self.keep_optimizers:
+                    state_dict['state'].pop('optimizers')
                 with fsdp_state_dict_type_context(state.model, state_dict_type='full'):
-                    state_dict = {'state': {'model': state.model.state_dict(), 'rng': reproducibility.get_rng_state()}}
+                    state_dict['state']['model'] = state.model.state_dict()
                     if dist.get_global_rank() == 0:
                         torch.save(state_dict, save_path)
+                if self.upload_to_object_store and dist.get_global_rank() == 0:
+                    remote_file_name = str(Path(save_dir) / Path(filename))
+                    self.remote_ud.upload_file(state=state, remote_file_name=remote_file_name, file_path=save_path, overwrite=self.overwrite)
 
 
 
