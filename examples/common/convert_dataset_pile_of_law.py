436871ea1a7f183be41325b5558d884e490cc1dd
Sasha Doubov <sasha@mosaicml.com>
2023-04-13 20:59:08 -0700

    fix num workers

--- examples/common/convert_dataset_pile_of_law.py
+++ examples/common/convert_dataset_pile_of_law.py
@@ -318,7 +318,10 @@ def _est_progress_denominator(total_samples: int, chars_per_sample: int,
 def build_dataloader(dataset, batch_size) -> DataLoader:
     # Multiple workers is only supported on linux machines
     if 'linux' in platform.platform().lower():
-        num_workers = min(64, dataset.hf_dataset.n_shards)  # type: ignore
+        if STREAMING_HF:
+            num_workers = min(64, dataset.hf_dataset.n_shards)  # type: ignore
+        else:
+            num_workers = 64
     else:
         num_workers = 0
 
