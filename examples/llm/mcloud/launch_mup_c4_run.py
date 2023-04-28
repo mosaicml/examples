30037803778660ba2ca9a2c2c5b673eaf7778e4a
Sasha Doubov <sasha@mosaicml.com>
2023-04-28 11:03:39 -0700

    user-friendly changes

--- examples/llm/mcloud/launch_mup_c4_run.py
+++ examples/llm/mcloud/launch_mup_c4_run.py
@@ -1,5 +1,6 @@
 from mcli.sdk import RunConfig, create_run
 import copy
+import math
 
 # or create directly in python with RunConfig(...)
 config = RunConfig.from_file('mcli-base-train-for-mup-c4-d-model-256.yaml')
@@ -18,8 +19,12 @@ for embed_scale in [1.0, 10.0]:
             print(updated_lr)
             print(dataset)
 
-            config.name = f'mgpt-40m-d-model-256-lr-{updated_lr:e}_data_{friendly_data}_embed-scale_{embed_scale}'
+            rounded_num = round(updated_lr, 2 - int(math.floor(math.log10(abs(updated_lr)))) - 1)
+            print(rounded_num)
 
+            config.name = f'mgpt-40m-d-256-lr-{updated_lr}-data-{friendly_data}-e-scale-{int(embed_scale)}'
+
+            print(config.name)
             # Update the parameters
             # deepcopy for safety
             run_params = copy.deepcopy(parameters)
@@ -30,6 +35,6 @@ for embed_scale in [1.0, 10.0]:
             print(config.parameters)
 
             # And run!
-            # run = create_run(config)
-            # print(f'Launching run {run.name} with lr {updated_lr} and data {friendly_data} and embed scale {embed_scale}')
-            # runs.append(run)
+            run = create_run(config)
+            print(f'Launching run {run.name} with lr {updated_lr} and data {friendly_data} and embed scale {embed_scale}')
+            runs.append(run)
