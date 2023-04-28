96da46fd603255e5bbfdfe69333063de4ebdf8d2
RR4787 <roshan@mosaicml.com>
2023-04-28 14:33:57 -0700

    wip

--- examples/inference/src/hf_instructor_model_class.py
+++ examples/inference/src/hf_instructor_model_class.py
@@ -1,9 +1,6 @@
 from InstructorEmbedding import INSTRUCTOR
 import torch
-import numpy as np
-import base64
 from typing import Dict, Any
-from utils import BaseModelHandler
 
 class BaseModelHandler:
 
@@ -28,15 +25,10 @@ class HFInstructorLargeModel(BaseModelHandler):
     def setup(self):
         self.model = INSTRUCTOR(self.MODEL_NAME)
 
-    def predict(self, **inputs: Dict[str, Any]):
-        if "instruction" not in inputs:
-            raise KeyError("Instruction key not in inputs")
+    def predict(self, **inputs):
+        # input_strings in format of [[<instruction>, <sentence>]]
+        if "input_strings" not in inputs:
+            raise KeyError("input_strings key not in inputs")
 
-        if "sentence" not in inputs:
-            raise KeyError("Sentence key not in inputs")
-
-        embeddings = self.model.encode([[inputs["instruction"], inputs["sentence"]]])
-        np_out = np.asarray(embeddings, dtype=np.float32)
-        output_bytes = bytes(base64.b64encode(np_out).decode('utf8'),
-                                encoding='utf8')
-        return output_bytes
+        embeddings = self.model.encode(inputs["input_strings"])
+        return embeddings.tolist()
