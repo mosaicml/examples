9c5ed3799ffc97acddc4d5447c253b8074db86b5
RR4787 <roshan@mosaicml.com>
2023-05-01 12:36:01 -0700

    add lint to makefile

--- examples/inference/src/hf_instructor_model_class.py
+++ examples/inference/src/hf_instructor_model_class.py
@@ -7,8 +7,9 @@ import torch
 from InstructorEmbedding import INSTRUCTOR
 
 
-"""Base class that all custom model handlers should inherit from"""
 class BaseModelHandler:
+    """Base class that all custom model handlers should inherit from."""
+
     def __init__(self) -> None:
         self.device = torch.cuda.current_device()
         self.setup()
@@ -20,22 +21,21 @@ class BaseModelHandler:
     def predict(self, **inputs: Dict[str, Any]):
         raise NotImplementedError
 
-"""Custom model handler class for instructor model"""
+
 class HFInstructorLargeModel(BaseModelHandler):
+    """Custom Hugging Face Instructor Model handler class."""
     MODEL_NAME = 'hkunlp/instructor-large'
+    model: INSTRUCTOR = None
 
     def setup(self):
-        """
-        Loads and instantiates the Hugging Face Large Instructor model.
-        """
+        """Loads and instantiates the Hugging Face Large Instructor model."""
         self.model = INSTRUCTOR(self.MODEL_NAME)
 
     def predict(self, **inputs: Dict[str, Any]):
-        """
-        Runs a forward pass with the given inputs. Inputs must be dictionary
-        that contains a 'input_strings' key with the value being in the following
-        format:
-            [<instruction>, <sentence>]
+        """Runs a forward pass with the given inputs.
+
+        Inputs must be dictionary that contains a 'input_strings' key with the
+        value being in the following format:     [<instruction>, <sentence>]
         """
         # input_strings in format of [[<instruction>, <sentence>]]
         if 'input_strings' not in inputs:
