c17cb96244dcd763017dd99e965cd38aac403cdf
RR4787 <roshan@mosaicml.com>
2023-05-01 12:16:10 -0700

    add docstrings and lint

--- examples/inference/src/hf_instructor_model_class.py
+++ examples/inference/src/hf_instructor_model_class.py
@@ -1,13 +1,18 @@
-from InstructorEmbedding import INSTRUCTOR
+# Copyright 2022 MosaicML Examples authors
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Any, Dict
+
 import torch
-from typing import Dict, Any
+from InstructorEmbedding import INSTRUCTOR
 
+
+"""Base class that all custom model handlers should inherit from"""
 class BaseModelHandler:
-
     def __init__(self) -> None:
         self.device = torch.cuda.current_device()
         self.setup()
-        print(f"DSInferenceModel initialized with device: {self.device}")
+        print(f'DSInferenceModel initialized with device: {self.device}')
 
     def setup(self):
         raise NotImplementedError
@@ -15,16 +20,26 @@ class BaseModelHandler:
     def predict(self, **inputs: Dict[str, Any]):
         raise NotImplementedError
 
+"""Custom model handler class for instructor model"""
 class HFInstructorLargeModel(BaseModelHandler):
     MODEL_NAME = 'hkunlp/instructor-large'
 
     def setup(self):
+        """
+        Loads and instantiates the Hugging Face Large Instructor model.
+        """
         self.model = INSTRUCTOR(self.MODEL_NAME)
 
     def predict(self, **inputs: Dict[str, Any]):
+        """
+        Runs a forward pass with the given inputs. Inputs must be dictionary
+        that contains a 'input_strings' key with the value being in the following
+        format:
+            [<instruction>, <sentence>]
+        """
         # input_strings in format of [[<instruction>, <sentence>]]
-        if "input_strings" not in inputs:
-            raise KeyError("input_strings key not in inputs")
-        
-        embeddings = self.model.encode(inputs["input_strings"])
+        if 'input_strings' not in inputs:
+            raise KeyError('input_strings key not in inputs')
+
+        embeddings = self.model.encode(inputs['input_strings'])
         return embeddings.tolist()
