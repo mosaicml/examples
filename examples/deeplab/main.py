09ef7c537e976c7a5c6ef7553d8c5a413e7fc915
Mihir Patel <mihir.v.patel7@gmail.com>
2023-05-02 13:25:40 -0700

    lint

--- examples/deeplab/main.py
+++ examples/deeplab/main.py
@@ -31,11 +31,13 @@ def build_logger(name: str, kwargs: dict):
 
 def main(config):
     reproducibility.seed_all(config.seed)
-
-    if config.device_train_microbatch_size == 'auto' and not torch.cuda.is_available():
+    auto_microbatching = config.device_train_microbatch_size == 'auto' 
+    if auto_microbatching and not torch.cuda.is_available():
         raise ValueError(
-            'device_train_microbatch_size="auto" requires training with a GPU; please specify device_train_microbatch_size as an integer'
+            'device_train_microbatch_size="auto" requires training with a GPU. Please specify'
+            ' device_train_microbatch_size as an integer'
         )
+
 
     # If using a recipe, update the config's loss name, eval and train resize sizes, and the max duration
     if config.recipe_name:
