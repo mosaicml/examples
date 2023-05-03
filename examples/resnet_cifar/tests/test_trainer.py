e97862c0c0b37b78b0d3ef2320164636040d694a
Mihir Patel <mihir.v.patel7@gmail.com>
2023-05-03 13:05:55 -0700

    lint

--- examples/resnet_cifar/tests/test_trainer.py
+++ examples/resnet_cifar/tests/test_trainer.py
@@ -27,32 +27,32 @@ def test_trainer(use_recipe):
     config.use_recipe = use_recipe
     config.seed = 1337 + 100 * use_recipe
 
-    with SynthClassificationDirectory() as tmp_datadir:
-        with SynthClassificationDirectory() as tmp_datadir2:
-            config.train_dataset.path = tmp_datadir
-            config.train_dataset.local = tmp_datadir
-            config.eval_dataset.path = tmp_datadir2
-            config.eval_dataset.local = tmp_datadir2
-            # Also save checkpoints in the temporary directory
-            config.save_folder = tmp_datadir
+    with SynthClassificationDirectory(
+    ) as tmp_datadir, SynthClassificationDirectory() as tmp_datadir2:
+        config.train_dataset.path = tmp_datadir
+        config.train_dataset.local = tmp_datadir
+        config.eval_dataset.path = tmp_datadir2
+        config.eval_dataset.local = tmp_datadir2
+        # Also save checkpoints in the temporary directory
+        config.save_folder = tmp_datadir
 
-            # Train
-            trainer1 = main(config)
-            model1 = trainer1.state.model.module
+        # Train
+        trainer1 = main(config)
+        model1 = trainer1.state.model.module
 
-            # TODO avoid tests taking a long time to exit without this
-            trainer1.state.dataloader._iterator._shutdown_workers()  # type: ignore
+        # TODO avoid tests taking a long time to exit without this
+        trainer1.state.dataloader._iterator._shutdown_workers()  # type: ignore
 
-            # Check that the checkpoint was saved
-            chkpt_path = os.path.join(tmp_datadir, 'ep0-ba1-rank0.pt')
-            assert os.path.isfile(chkpt_path)
+        # Check that the checkpoint was saved
+        chkpt_path = os.path.join(tmp_datadir, 'ep0-ba1-rank0.pt')
+        assert os.path.isfile(chkpt_path)
 
-            # Check that the checkpoint was loaded by comparing model weights
-            config.load_path = chkpt_path
-            config.is_train = False
-            config.seed += 10  # change seed
-            trainer2 = main(config)
-            model2 = trainer2.state.model.module
+        # Check that the checkpoint was loaded by comparing model weights
+        config.load_path = chkpt_path
+        config.is_train = False
+        config.seed += 10  # change seed
+        trainer2 = main(config)
+        model2 = trainer2.state.model.module
 
-            for param1, param2 in zip(model1.parameters(), model2.parameters()):
-                torch.testing.assert_close(param1, param2)
+        for param1, param2 in zip(model1.parameters(), model2.parameters()):
+            torch.testing.assert_close(param1, param2)
