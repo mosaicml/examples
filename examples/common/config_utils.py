dd1a3ef114de84610f3f2c4771b6d66752cc63a3
Mihir Patel <mihir.v.patel7@gmail.com>
2023-05-02 17:51:19 -0700

    update pr

--- examples/common/config_utils.py
+++ examples/common/config_utils.py
@@ -10,14 +10,23 @@ from omegaconf import OmegaConf as om
 
 
 def update_batch_size_info(cfg: DictConfig):
+    global_batch_size, device_microbatch_size = cfg.global_train_batch_size, cfg.device_train_microbatch_size
+    if global_batch_size % dist.get_world_size() != 0:
+        raise ValueError(
+            f'Global batch size {global_batch_size} is not divisible by {dist.get_world_size()} '
+            'as a result, the batch size would be truncated, please adjust `global_batch_size` '
+            f'to be divisible by world size, {dist.get_world_size()}.')
+    device_train_batch_size = global_batch_size// dist.get_world_size()
+    if isinstance(device_microbatch_size, int):
+        if device_microbatch_size > device_train_batch_size:
+            print(
+                f'WARNING: device_train_microbatch_size > device_train_batch_size, '
+                f'will be reduced from {device_microbatch_size} -> {device_train_batch_size}.'
+            )
+            device_microbatch_size = device_train_batch_size
     cfg.n_gpus = dist.get_world_size()
-    # Safely set `device_eval_batch_size` if not provided by user
-    if 'device_eval_batch_size' not in cfg:
-        if cfg.device_train_microbatch_size == 'auto':
-            cfg.device_eval_batch_size = 1  # TODO debug auto eval microbatching
-        else:
-            cfg.device_eval_batch_size = cfg.device_train_microbatch_size
-    return cfg
+    cfg.device_train_batch_size = device_train_batch_size
+    cfg.device_train_microbatch_size = device_microbatch_size
 
 
 def log_config(cfg: DictConfig):
