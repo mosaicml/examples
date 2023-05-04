9ff93c64df51d20280c3af0289dccb9a716f96d9
Sam Havens <sam@mosaicml.com>
2023-05-04 02:14:28 -0700

    add dataset

--- examples/llm/src/data/finetuning/_tasks.py
+++ examples/llm/src/data/finetuning/_tasks.py
@@ -205,7 +205,8 @@ def dolly_tokenize_function(inp: Dict, tokenizer: Tokenizer):
 
 
 @dataset_constructor.register('sam-mosaic/vicuna_alpaca_hc3_chatml',
-                              'sam-mosaic/dolly_hhrlhf', 'sam-mosaic/evol_chat')
+                              'sam-mosaic/dolly_hhrlhf', 'sam-mosaic/evol_chat',
+                              'sam-mosaic/hhrlhf_evol_chatml')
 def dolly_chatml_tokenize_function(inp: Dict, tokenizer: Tokenizer):
     """Already split, just tokenize."""
     return tokenizer(
