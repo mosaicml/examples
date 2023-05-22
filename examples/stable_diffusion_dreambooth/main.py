e37d79874dc9f7c2409e076a5155ff7d4c9d445c
Austin <A-Jacobson@users.noreply.github.com>
2023-05-22 15:12:27 -0700

    Sd dreambooth bugfix (#332)
    * pass kwargs correctly

--- examples/stable_diffusion_dreambooth/main.py
+++ examples/stable_diffusion_dreambooth/main.py
@@ -80,7 +80,7 @@ def main(config: DictConfig):  # type: ignore
         center_crop=config.dataset.center_crop,
         tokenizer=model.tokenizer,
         batch_size=config.train_device_batch_size,
-        dataloader_kwargs=config.dataset.dataloader_kwargs)
+        **config.dataset.dataloader_kwargs)
 
     # Optimizer
     print('Building optimizer and learning rate scheduler')
