0989cafdd5ae36313a233661ed4d697a5b04a561
Daniel King <daniel@mosaicml.com>
2023-06-03 01:30:49 +0000

    fix

--- examples/end-to-end-examples/sec_10k_qa/deployment_download_helper.py
+++ examples/end-to-end-examples/sec_10k_qa/deployment_download_helper.py
@@ -1,11 +1,24 @@
 # Copyright 2022 MosaicML Examples authors
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+
 from composer.utils import get_file
 
 
 def download_model(remote_uri: str):
-    get_file(
-        path=remote_uri,
-        destination='/downloaded_hf_checkpoint/',
-    )
+    files = [
+        'config.json',
+        'pytorch_model.bin',
+        'special_tokens_map.json',
+        'tokenizer_config.json',
+        'tokenizer.json',
+        'generation_config.json',
+        'merges.txt',
+        'vocab.json',
+    ]
+    for file in files:
+        get_file(
+            path=os.path.join(remote_uri, file),
+            destination=os.path.join('/downloaded_hf_checkpoint/', file),
+        )
