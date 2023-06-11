ed9fc88458fe374a576d3d7db42fb6ad5da9d1e8
Daniel King <daniel@mosaicml.com>
2023-06-11 01:11:14 -0700

    app.py docs

--- examples/end-to-end-examples/sec_10k_qa/app.py
+++ examples/end-to-end-examples/sec_10k_qa/app.py
@@ -33,8 +33,8 @@ elif args.dataset_subset == 'large_full':
 else:
     raise ValueError(f"Unknown dataset subset {args.dataset_subset}")
 
-with open(
-        os.path.join(os.path.dirname(path_to_current_file), ticker_file_name), 'r') as _json_file:
+with open(os.path.join(os.path.dirname(path_to_current_file), ticker_file_name),
+          'r') as _json_file:
     ticker_to_years = {
         k: sorted(v, key=lambda x: int(x))
         for k, v in json.load(_json_file).items()
@@ -42,6 +42,14 @@ with open(
 
 
 def clean_response(input_text: str) -> str:
+    """Clean the response from the model.
+
+    Args:
+        input_text (str): The response from the model.
+
+    Returns:
+        str: The cleaned response.
+    """
     input_text = input_text.strip('\n')
 
     context_prefix = "Context:"
@@ -75,7 +83,7 @@ def greet(
                                    f'sec_{year}_txt.txt')
     os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
 
-    print(f"Getting file {remote_file_path} to {local_file_path}")
+    print(f"Downloading file from {remote_file_path} to {local_file_path}")
 
     if ticker not in ticker_to_years:
         return f"Invalid ticker {ticker} see test_ticker_to_years_{{large|small}}.json for all the tickers in the test set", '', ''
@@ -89,13 +97,18 @@ def greet(
     with open(local_file_path, 'r') as f:
         doc = f.read()
 
+    # Component for splitting the long document until chunks that fit in the embedding model
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=0,
-        separators=["(?<=\\.) (?=\\s)", "(?<=\\?) (?=\\s)", "(?<=\\!) (?=\\s)", '\n', ' ', ''],
+        separators=[
+            "(?<=\\.) (?=\\s)", "(?<=\\?) (?=\\s)", "(?<=\\!) (?=\\s)", '\n',
+            ' ', ''
+        ], # Split on periods, question marks, exclamation marks, new lines, spaces, and empty strings, in the order
     )
     split_doc = text_splitter.split_documents([Document(page_content=doc)])
 
+    # Component for embedding the text, using the inference endpoint deployed using MosaicML
     embeddings = MosaicMLInstructorEmbeddings(
         endpoint_url=args.embedding_endpoint_url,
         embed_instruction='Represent the Financial statement for retrieval: ',
@@ -103,6 +116,7 @@ def greet(
         'Represent the Financial question for retrieving supporting documents: ',
     )
 
+    # Embed the text in batches, to avoid exceeding the maximum payload size
     batches = []
     current_char_count = 0
     current_batch = []
@@ -125,25 +139,29 @@ def greet(
         text_embeddings.extend(
             list(zip([d.page_content for d in batch], batch_embeddings)))
 
+    # Component for storing the embeddings in a vector store, using FAISS
     vector_store = FAISS.from_embeddings(
         text_embeddings=text_embeddings,
         embedding=embeddings,
     )
     retriever = vector_store.as_retriever(
         search_type='similarity',
-        search_kwargs={'k': 8},
+        search_kwargs={'k': 8}, # retrieve the top 8 most similar documents, this can be tweaked
     )
 
+    # Component for generating the response, using the inference endpoint deployed using MosaicML
     llm = MosaicML(
         endpoint_url=args.llm_endpoint_url,
         inject_instruction_format=True,
         model_kwargs={
-            'max_new_tokens': 200,
-            'do_sample': False,
+            'max_new_tokens': 200, # maximum number of response tokens to generate
+            'do_sample': False, # perform greedy decoding
             'use_cache': True
+            # other HuggingFace generation parameters can be set as kwargs here to experiment with different decoding parameters
         },
     )
 
+    # Prompt template for the query
     answer_question_string_template = (
         f"Use the following pieces of context to answer the question at the end. The context is from a {year} financial document about {ticker}. If the question cannot be answered accurately from the provided context, say 'The question cannot be answered from the retrieved documents.'."
         "\n{context}"
@@ -154,37 +172,45 @@ def greet(
         template=answer_question_string_template,
         input_variables=["context", "question"])
 
+    # Component connecting the LLM with the prompt template
     llm_chain = LLMChain(
         llm=llm,
         prompt=answer_question_prompt_template,
     )
 
+    # Component connecting the context documents with the LLM chain
     stuff_documents_chain = StuffDocumentsChain(
         llm_chain=llm_chain,
         document_variable_name='context',
         document_prompt=DOCUMENT_PROMPT,
     )
 
+    # Complete component for retrieval question answering
     answer_qa = RetrievalQA(
         retriever=retriever,
         combine_documents_chain=stuff_documents_chain,
         return_source_documents=True,
     )
 
+    # Get the response
     answer_response = answer_qa(query)
 
+    # Clean the response
     answer = clean_response(answer_response['result'].lstrip('\n'))
 
+    # Return the answer and the retrieved documents
     return answer, '\n\n'.join(
         [d.page_content for d in answer_response['source_documents']])
 
+
 if args.dataset_subset == 'small_full':
     default_ticker = 'FRTG'
-    default_year='2020'
+    default_year = '2020'
 elif args.dataset_subset == 'large_full':
     default_ticker = 'UPST'
-    default_year='2020'
+    default_year = '2020'
 
+# Simple gradio application for querying the model
 with gr.Blocks() as demo:
     ticker = gr.Textbox(label="Ticker", value=default_ticker)
     year = gr.Textbox(label="Year", value=default_year)
