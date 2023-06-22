0d6df75fe0bee289b0cec5488df208918ec11fbb
Vincent Chen <vincent@mosaicml.com>
2023-06-21 17:46:59 -0700

    stage 1 fixed script inputs

--- examples/end-to-end-examples/support_chatbot/upload_data.py
+++ examples/end-to-end-examples/support_chatbot/upload_data.py
@@ -3,34 +3,35 @@ import os
 from typing import Any, Dict
 from composer.utils import maybe_create_object_store_from_uri, parse_uri
 
-def main(file_path: str, cloud_folder_for_upload: str):
+def main(file_path: str, dataset_subset: str):
     """
     Main function to upload a JSONL file to a cloud storage.
 
     Args:
         file_path (str): The local path of the JSONL file to be uploaded.
-        cloud_folder_for_upload (str): The cloud storage URI where the file will be uploaded.
+        dataset_subset (str): The subset of the dataset to be uploaded.
 
     """
     # Create the object store to use for uploading data
-    object_store = maybe_create_object_store_from_uri(cloud_folder_for_upload)
-    _, _, cloud_folder_prefix = parse_uri(cloud_folder_for_upload)
+    object_store = maybe_create_object_store_from_uri(file_path)
+    _, _, cloud_folder_prefix = parse_uri(file_path)
 
     # Upload the file to the cloud storage
-    object_store.upload_object(object_name=os.path.join(cloud_folder_prefix, os.path.basename(file_path)),
+    object_store.upload_object(object_name=os.path.join(cloud_folder_prefix, dataset_subset),
                                filename=file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Upload JSONL data to cloud storage')
     parser.add_argument(
-        '--file_path',
+        '--folder_for_upload',
         type=str,
         help='The path of the JSONL file to upload')
     parser.add_argument(
-        '--cloud_folder_for_upload',
+        '--dataset_subset',
         type=str,
-        help='Cloud storage prefix to upload the processed data to')
+        default='small_full',
+        help='Dataset subset to upload. Options are large_full and small_full')
     args = parser.parse_args()
 
-    main(args.file_path, args.cloud_folder_for_upload)
+    main(args.folder_for_upload, args.dataset_subset)
