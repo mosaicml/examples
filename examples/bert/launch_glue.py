427724ff52071491ade0fe8037d77944e3c26669
Zack Ankner <zackankner@gmail.com>
2023-07-01 12:53:09 -0700

    dunno

--- examples/bert/launch_glue.py
+++ examples/bert/launch_glue.py
@@ -4,8 +4,8 @@ import copy
 from mcli import RunConfig, create_run
 
 
-def build_checkpoint_path(scheduler, init_rate, final_rate, seed, ckpt):
-    return f"s3://mosaicml-internal-checkpoints-shared/zack/paper-mlm-schedule/schedule-{scheduler}-{init_rate}-{final_rate}-seed-{seed}/ep0-ba{ckpt}-rank0.pt"
+def build_checkpoint_path(scheduler, init_rate, final_rate, seed, ckpt, model):
+    return f"s3://mosaicml-internal-checkpoints-shared/zack/paper-mlm-schedule/{'large-' if model == 'large' else ''}schedule-{scheduler}-{init_rate}-{final_rate}-seed-{seed}/ep0-ba{ckpt}-rank0.pt"
 
 
 def main(args):
@@ -14,6 +14,10 @@ def main(args):
 
     if args.mode == "final":
         ckpts = [70_000]
+    elif args.mode == "speed-up":
+        ckpts = [*range(0, 69_999, 10_000)][1:]
+    elif args.ckpt is not None:
+        ckpts = [args.ckpt]
     else:
         raise ValueError(f"Unknown mode: {args.mode}")
 
@@ -23,26 +27,54 @@ def main(args):
 
             run.name = f"{args.scheduler}-{args.initial_mlm_rate}-to-{args.final_mlm_rate}-seed-{seed}-glue"
             run.name = run.name.replace(".", "")
-            run.parameters[
-                "base_run_name"] = f"schedule-{args.scheduler}-{args.initial_mlm_rate}-{args.final_mlm_rate}-seed-{seed}"
+            rn = f"schedule-{args.scheduler}-{args.initial_mlm_rate}-{args.final_mlm_rate}-seed-{seed}-ckpt-{ckpt}"
+            if args.model == "large":
+                rn = "large-" + rn
+            run.parameters["base_run_name"] = rn
+
+            # if args.mode == "speed-up" or args.priority == "low":
+            #     run.scheduling = {"priority": "low"}
 
             run.parameters[
                 "starting_checkpoint_load_path"] = build_checkpoint_path(
                     args.scheduler, args.initial_mlm_rate, args.final_mlm_rate,
-                    seed, ckpt)
+                    seed, ckpt, args.model)
 
             run.parameters["loggers"]["wandb"]["tags"] = [
                 "glue", args.scheduler, f"initial-{args.initial_mlm_rate}",
                 f"final-{args.final_mlm_rate}", f"og-seed-{seed}",
                 f"bert-{args.model}"
             ]
-            if args.mode == "final":
+            if args.mode == "final" or args.mode == "ckpt":
                 run.parameters["loggers"]["wandb"]["tags"].append("best-ckpt")
+            elif args.mode == "speed-up":
+                run.parameters["loggers"]["wandb"]["tags"].append("speed-up")
 
             run.parameters["loggers"]["wandb"]["group"] = "-".join([
                 args.scheduler, f"initial-{args.initial_mlm_rate}",
-                f"final-{args.final_mlm_rate}", f"og-seed-{seed}"
+                f"final-{args.final_mlm_rate}", f"og-seed-{seed}", f"ckpt-{ckpt}"
             ])
+
+            # Setting all ocurrences of specific model
+            model_name_fmt = f"bert-{args.model}-uncased"
+            run.parameters["tokenizer_name"] = model_name_fmt
+            run.parameters["model"]["pretrained_model_name"] = model_name_fmt
+            run.parameters["model"]["tokenizer_name"] = model_name_fmt
+
+            # Handling model config if bert large
+            if args.model == "large":
+                model_config = {
+                    "attention_probs_dropout_prob": 0.1,
+                    "hidden_size": 1024,
+                    "intermediate_size": 4096,
+                    "num_attention_heads": 16,
+                    "num_hidden_layers": 24
+                }
+                run.parameters["model"]["model_config"] = model_config
+
+            run.cluster = args.cluster
+            if run.cluster not in ["r1z1", "r8z6"]:
+                run.gpu_type = "a100_40gb"
 
             create_run(run)
 
@@ -53,7 +85,10 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, required=True)
     parser.add_argument("--initial-mlm-rate", type=float, required=True)
     parser.add_argument("--final-mlm-rate", type=float, required=True)
+    parser.add_argument("--cluster", type=str, default="r1z1")
     parser.add_argument("--mode", type=str, default="final")
+    parser.add_argument("--priority", type=str, default="medium")
+    parser.add_argument("--ckpt", type=int, default=None)
     args = parser.parse_args()
 
     main(args)
--- examples/bert/launch_pretrain.py
+++ examples/bert/launch_pretrain.py
@@ -8,15 +8,20 @@ def main(args):
     cfg_path = "yamls/main/mlm_scheduled/base.yaml"
     config = RunConfig.from_file(cfg_path)
 
-    for seed in [17, 2048, 3047]:
+    for seed in args.seeds:
         run = copy.deepcopy(config)
 
         run.name = f"{args.scheduler}-{args.initial_mlm_rate}-to-{args.final_mlm_rate}-seed-{seed}"
         run.name = run.name.replace(".", "")
-        run.parameters[
-            "run_name"] = f"schedule-{args.scheduler}-{args.initial_mlm_rate}-{args.final_mlm_rate}"
+        rn = f"schedule-{args.scheduler}-{args.initial_mlm_rate}-{args.final_mlm_rate}"
+        if args.model == "large":
+            rn = "shortenedlarge-" + rn
+        if args.subset_mask:
+            rn = "subset-mask-" + rn
+        run.parameters["run_name"] = rn
 
         run.parameters["seed"] = seed
+        run.parameters["train_loader"]["dataset"]["shuffle_seed"] = seed
 
         run.parameters["train_loader"]["dataset"]["mlm_schedule"][
             "name"] = args.scheduler
@@ -24,25 +29,57 @@ def main(args):
             "initial_mlm_rate"] = args.initial_mlm_rate
         run.parameters["train_loader"]["dataset"]["mlm_schedule"][
             "final_mlm_rate"] = args.final_mlm_rate
+        run.parameters["train_loader"]["dataset"]["mlm_schedule"][
+            "subset_masking_rate"] = 0.15 if args.subset_mask else None
 
         run.parameters["loggers"]["wandb"]["tags"] = [
             "pretraining", args.scheduler, f"initial-{args.initial_mlm_rate}",
             f"final-{args.final_mlm_rate}", f"bert-{args.model}"
         ]
-
         run.parameters["loggers"]["wandb"]["group"] = "-".join([
             "pretraining", args.scheduler, f"initial-{args.initial_mlm_rate}",
             f"final-{args.final_mlm_rate}"
         ])
 
+        if args.subset_mask:
+            run.parameters["loggers"]["wandb"]["tags"].append("subset-mask")
+            run.parameters["loggers"]["wandb"]["group"] = "subset-mask-" + run.parameters["loggers"]["wandb"]["group"]
+
+
         if args.autoresume:
             run.scheduling = {"resumable": True, "priority": "low"}
             run.parameters["autoresume"] = True
-        
+
         run.cluster = args.cluster
         if run.cluster not in ["r1z1", "r8z6"]:
             run.gpu_type = "a100_40gb"
             run.gpu_num = 16
+
+        # Setting all ocurrences of specific model
+        model_name_fmt = f"bert-{args.model}-uncased"
+        run.parameters["tokenizer_name"] = model_name_fmt
+        run.parameters["model"]["pretrained_model_name"] = model_name_fmt
+        run.parameters["model"]["tokenizer_name"] = model_name_fmt
+        run.parameters["train_loader"]["dataset"][
+            "tokenizer_name"] = model_name_fmt
+        run.parameters["eval_loader"]["dataset"][
+            "tokenizer_name"] = model_name_fmt
+
+        # Handling model config if bert large
+        if args.model == "large":
+            run.parameters
+            model_config = {
+                "attention_probs_dropout_prob": 0.1,
+                "hidden_size": 1024,
+                "intermediate_size": 4096,
+                "num_attention_heads": 16,
+                "num_hidden_layers": 24
+            }
+            run.parameters["model"]["model_config"] = model_config
+
+            run.parameters["optimizer"]["lr"] = 2.0e-4
+
+            run.parameters["max_duration"] = "286720000sp"
 
         create_run(run)
 
@@ -55,6 +92,11 @@ if __name__ == "__main__":
     parser.add_argument("--final-mlm-rate", type=float, required=True)
     parser.add_argument("--autoresume", action="store_true")
     parser.add_argument("--cluster", default="r1z1")
+    parser.add_argument("--seeds",
+                        nargs="+",
+                        type=int,
+                        default=[17, 2048, 3047])
+    parser.add_argument("--subset-mask", action="store_true")
     args = parser.parse_args()
 
     main(args)
